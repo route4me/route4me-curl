549283d2c3eb75a7d33c5ff482ed74b259d5ecb4
Amazos <olegi_g@yahoo.com>
2016-06-15 15:44:05 +0400

    added reverse_geocoding example

--- Windows/Geocoding/reverse_geocoding.bat
+++ Windows/Geocoding/reverse_geocoding.bat
@@ -1,8 +1,9 @@
 @ECHO OFF
+
 SET url=http://www.route4me.com/api/geocoder.php
 SET apikey=11111111111111111111111111111111
 SET format=xml
-SET addrs="Los20%Angeles20%International20%Airport,20%CA"
+SET addrs="42.35863,-71.05670"
 
 ECHO ON
 
