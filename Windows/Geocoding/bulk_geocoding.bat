5f7c2ed0fc08b034db3957c4fb4e7dfcd75e8a4f
Amazos <olegi_g@yahoo.com>
2016-06-16 19:39:25 +0400

    update bulk_geocode

---
+++ Windows/Geocoding/bulk_geocoding.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+SET url=http://www.route4me.com/actions/upload/json-geocode.php
+SET apikey=11111111111111111111111111111111
+
+ECHO ON
+
+:: This task requires clarification- it refuses to work because API key has exceeded its maximum number of requests
+
+curl -o file1.txt -g -X POST -d '@bulk_geocoding_data.json' "%URL%?api_key=%apikey%" 
+
+timeout /t 30
