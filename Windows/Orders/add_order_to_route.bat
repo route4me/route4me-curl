08dd6728ef01a67c166a39ddece8a1966d8238f2
Amazos <olegi_g@yahoo.com>
2016-06-18 22:12:51 +0400

    update add_order_to_route example

--- Windows/Orders/add_order_to_route.bat
+++ Windows/Orders/add_order_to_route.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-SET url=http://broono.com/api.v4/route.php
+SET url=http://route4me.com/api.v4/route.php
 SET apikey=33383266363136302D623934312D3131
 SET rdirect=0
 SET routeid=CEAA81617489EC7F8972372F6248946D
