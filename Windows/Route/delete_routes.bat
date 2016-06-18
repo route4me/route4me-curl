d8b270bedccd9060c456aca70b9b690ae4a7e729
Amazos <olegi_g@yahoo.com>
2016-06-18 15:02:12 +0400

    update delete_routes example

--- Windows/Route/delete_routes.bat
+++ Windows/Route/delete_routes.bat
@@ -1,8 +1,12 @@
 @ECHO OFF
-SET url=https://www.route4me.com
+SET url=https://www.route4me.com/api.v4/route.php
 SET apikey=11111111111111111111111111111111
+SET "routeid=270CB108F227AD6E11827FC08EE4E2D7,C340E3F6AF28E20EFAE0FBE51759C338"
 
 ECHO ON
-curl -o file1.txt -g -k -X POST -d '@delete_routes_data.json' "%URL%/actions/delete_routes.php?api_key=%apikey%" 
+
+:: Example demonstrates the process of deleting specified routes by ids sent with HTTP DELETE data
+
+curl -o file1.txt -g -k -X DELETE "%url%?api_key=%apikey%&route_id=%routeid%" 
 
 timeout /t 30
