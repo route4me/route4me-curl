87460db211cdb2cc586e0bed8af5ec2d0727ce18
Amazos <olegi_g@yahoo.com>
2016-06-18 22:49:39 +0400

    update_add_order example

--- Linux/Orders/add_order.sh
+++ Linux/Orders/add_order.sh
@@ -3,7 +3,7 @@
 url=https://www.route4me.com/api.v4/order.php
 apikey=11111111111111111111111111111111
 
-# The example demonstrates process of adding an address to the order by sending HTPP POST data.
+# The example demonstrates process of creating an order by sending HTPP POST data.
 
 curl -o file1.txt -k -g -X POST -H "Content-Type: application/json" -d "@add_order_data.json" "$url?api_key=$apikey"
 
--- Windows/Orders/add_order.bat
+++ Windows/Orders/add_order.bat
@@ -5,7 +5,7 @@ SET apikey=11111111111111111111111111111111
 
 ECHO ON
 
-:: The example demonstrates process of adding an address to the order by sending HTPP POST data
+:: The example demonstrates process of creating an order by sending HTPP POST data
 
 curl -o file1.txt -k -g -X POST -H "Content-Type: application/json" -d "@add_order_data.json" "%url%?api_key=%apikey%"
 
