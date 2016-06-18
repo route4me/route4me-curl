c9590867aff4bafad8d886d909b31c438c28fa86
Amazos <olegi_g@yahoo.com>
2016-06-18 22:47:12 +0400

    upload delete_order example

---
+++ Linux/Orders/delete_order.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+url=http://www.broono.com/api.v4/order.php
+apikey=11111111111111111111111111111111
+
+# The example demonstrates process of deleting an order by sending HTPP DELETE data
+
+curl -o file1.txt -g -X DELETE -H "Content-Type: application/json" -d "@delete_order_data.json" "$url?api_key=$apikey"
+
+echo "Finished..."
+
+sleep 15
