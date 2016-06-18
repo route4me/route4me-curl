c77621270856eecd9eab374fae5cdc4ac4b43dd1
Amazos <olegi_g@yahoo.com>
2016-06-18 15:18:04 +0400

    update delete_routes example

--- Linux/Route/delete_routes.sh
+++ Linux/Route/delete_routes.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-url=https://www.route4me.com/actions/delete_routes.php
+url=https://www.route4me.com/api.v4/route.php
 apikey=11111111111111111111111111111111
+routeid="270CB108F227AD6E11827FC08EE4E2D7,C340E3F6AF28E20EFAE0FBE51759C338"
 
-curl -o file1.txt -g -k -X POST -d "@delete_routes_data.json" "$url?api_key=$apikey" 
+# Example demonstrates the process of deleting specified routes by ids sent with HTTP DELETE data
+
+curl -o file1.txt -g -k -X DELETE "$url?api_key=$apikey&route_id=$routeid" 
 
 echo "Finished..."
 
