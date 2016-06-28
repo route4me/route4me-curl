f00f97a61231da8e6c13233d5013e6992e3faee4
Amazos <olegi_g@yahoo.com>
2016-06-28 19:52:32 +0400

    update Orders group examples

--- Windows/Optimizations/get_optimization_single.bat
+++ Windows/Optimizations/get_optimization_single.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 SET url=https://www.route4me.com
 SET apikey=11111111111111111111111111111111
-SET optprobid=07372F2CF3814EC6DFFAFE92E22771AA
+SET optprobid=8034E6A76E8518D2F9E49171E6A89896
 
 ECHO ON
 curl -o file1.txt -g -X GET -k "%URL%/api.v4/optimization_problem.php?api_key=%apikey%&optimization_problem_id=%optprobid%" 
--- Windows/Orders/add_order_to_optimization.bat
+++ Windows/Orders/add_order_to_optimization.bat
@@ -1,13 +1,13 @@
 @ECHO OFF
 
 SET url=http://route4me.com/api.v4/optimization_problem.php
-SET apikey=33383266363136302D623934312D3131
+SET apikey=11111111111111111111111111111111
 SET rdirect=0
-SET optid=E0883C1A2C7B3AAA9397BDDF90C9CFF8
+SET optid=8034E6A76E8518D2F9E49171E6A89896
 
 ECHO ON
 
-:: The example demonstrates process of adding an order to the optimization by sending HTPP PUT data
+:: The example refers to the process of adding an order to an optimization by sending HTPP PUT data
 
 curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@add_order_to_route_data.json" "%url%?api_key=%apikey%&redirect=%rdirect%&optimization_problem_id=%optid%"
 
