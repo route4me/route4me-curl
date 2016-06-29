23091a5357c65b39a133f65013c27223b72ec814
Amazos <olegi_g@yahoo.com>
2016-06-29 19:14:28 +0400

    added Orders2Optimizations group in UseCase  folder

---
+++ UseCase/Orders2Optimization/add_order_to_optimization.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+SET url=http://broono.com/api.v4/optimization_problem.php
+SET apikey=11111111111111111111111111111111
+SET rdirect=0
+SET optid=896C4E5466213841BB9AEF3EEF5D7F48
+
+ECHO ON
+
+:: The example refers to the process of adding an order to an optimization by sending HTPP PUT data
+
+curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@add_order_to_route_data.json" "%url%?api_key=%apikey%&redirect=%rdirect%&optimization_problem_id=%optid%"
+
+timeout /t 30
