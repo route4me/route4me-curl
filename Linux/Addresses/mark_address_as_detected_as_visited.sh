11c544288fe64d9fbe277f76816589d6db069632
Amazos <olegi_g@yahoo.com>
2016-07-07 12:11:30 +0400

    update Address group examples

--- Linux/Addresses/mark_address_as_detected_as_visited.sh
+++ Linux/Addresses/mark_address_as_detected_as_visited.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-url=http://www.route4me.com/api.v4/address.php
+url=https://www.route4me.com/api.v4/address.php
 apikey=11111111111111111111111111111111
 routeid=241466F15515D67D3F951E2DA38DE76D
 routedestinationid=167899269
 
-:: is_visited=1 means address was visited, is_departed=0 --- not visited
+:: is_visited=1 means address was visited, is_visited=0 --- not visited
 
-curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@mark_address_as_detected_as_departed_data.json" "$url?api_key=$apikey%&route_id=$routeid&route_destination_id=$routedestinationid"
+curl -o file1.txt -k -g -X PUT -H "Content-Type: application/json" -d "@mark_address_as_detected_as_visited_data.json" "$url?api_key=$apikey%&route_id=$routeid&route_destination_id=$routedestinationid"
 
 echo "Finished..."
 
--- Windows/Addresses/mark_address_as_detected_as_departed.bat
+++ Windows/Addresses/mark_address_as_detected_as_departed.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-SET url=http://www.route4me.com/api.v4/address.php
+SET url=https://www.route4me.com/api.v4/address.php
 SET apikey=11111111111111111111111111111111
 SET routeid=241466F15515D67D3F951E2DA38DE76D
 SET routedestinationid=167899269
@@ -8,6 +8,6 @@ ECHO ON
 
 :: is_departed=1 means address was departed, is_departed=0 --- not departed
 
-curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@mark_address_as_detected_as_departed_data.json" "%url%?api_key=%apikey%&route_id=%routeid%&route_destination_id=%routedestinationid%"
+curl -o file1.txt -k -g -X PUT -H "Content-Type: application/json" -d "@mark_address_as_detected_as_departed_data.json" "%url%?api_key=%apikey%&route_id=%routeid%&route_destination_id=%routedestinationid%"
 
 timeout /t 30
