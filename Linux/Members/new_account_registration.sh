764e285c0e096ee0a07897bd27ed34e58a058faa
Amazos <olegi_g@yahoo.com>
2016-07-16 23:03:54 +0400

    update New Account Registration account

--- Linux/Members/new_account_registration.sh
+++ Linux/Members/new_account_registration.sh
@@ -2,11 +2,13 @@
 
 url=https://www.route4me.com/actions/register_action.php
 apikey=11111111111111111111111111111111
+plan=enterprise_plan
 
-# This task requires email provider
+# The example refers to a registration new Route4Me account.
 
-curl -o file1.txt -g -k -X POST -d "@new_account_registration_data.json" "$url?api_key=$apikey"
+curl -o file1.txt -k -X POST -H "Content-Type: multipart/form-data;" -F "strIndustry=Gifting" -F "strFirstName=Olman" -F "strLastName=Oland" -F "strEmail=ololol@outlook.com" -F "format=json" -F "chkTerms=1" -F "device_type=web" -F "strPassword_1=111111" -F "strPassword_2=111111" "$url?plan=$plan"
 
 echo "Finished..."
 
 sleep 15
+
--- Windows/Members/new_account_registration.bat
+++ Windows/Members/new_account_registration.bat
@@ -1,10 +1,13 @@
 @ECHO OFF
-SET url=https://www.route4me.com
+
+SET url=https://www.route4me.com/actions/register_action.php
 SET apikey=11111111111111111111111111111111
+SET plan=enterprise_plan
+
+::The example refers to a registration new Route4Me account.
 
 ECHO ON
 
-:: This task requires email provider
-curl -o file1.txt -g -k -X POST -d '@new_account_registration_data.json' "%URL%/actions/register_action.php?api_key=%apikey%" 
+curl -vvv -k -X POST -H "Content-Type: multipart/form-data;" -F "strIndustry=Gifting" -F "strFirstName=Olman" -F "strLastName=Oland" -F "strEmail=ololol@outlook.com" -F "format=json" -F "chkTerms=1" -F "device_type=web" -F "strPassword_1=111111" -F "strPassword_2=111111" "%url%?plan=%plan%"
 
 timeout /t 30
