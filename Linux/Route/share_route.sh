e2e63cf516449e6a3f42760febc4a5637bcb685a
Amazos <olegi_g@yahoo.com>
2016-07-22 18:12:27 +0400

    update Shear Route example

--- Linux/Route/share_route.sh
+++ Linux/Route/share_route.sh
@@ -5,9 +5,9 @@ apikey=11111111111111111111111111111111
 routeid=3A2DD89E6E1A044B2098AD1313E3138C
 frmt=json
 
-# will work only in OS with email provider
+# Example refers to the process of shearing a route by email
 
-curl -o file1.txt -g -X POST -k -d 'email_for_route_sharing.json' "$url?api_key=$apikey&route_id=$routeid&response_format=$frmt" 
+curl -o file1.txt -g -X POST -k -H "Content-Type: multipart/form-data;" -F "recipient_email=oooooo@gmail.com" "$url?api_key=$apikey&route_id=$routeid&response_format=$frmt" 
 
 echo "Finished.."
 
--- Windows/Route/share_route.bat
+++ Windows/Route/share_route.bat
@@ -1,10 +1,13 @@
 @ECHO OFF
-SET url=https://www.route4me.com
+
+SET url=https://www.route4me.com/actions/route/share_route.php
 SET apikey=11111111111111111111111111111111
 SET routeid=3A2DD89E6E1A044B2098AD1313E3138C
 
 ECHO ON
-::will work only in OS with email provider
-curl -o file1.txt -g -X POST -k -d 'email_for_route_sharing.json' "%URL%/actions/route/share_route.php?api_key=%apikey%&route_id=%routeid%&response_format=json" 
+
+:: Example refers to the process of shearing a route by email
+
+curl -o file1.txt -g -X POST -k -H "Content-Type: multipart/form-data;" -F "recipient_email=oooooo@gmail.com" "%url%?api_key=%apikey%&route_id=%routeid%&response_format=json" 
 
 timeout /t 30
