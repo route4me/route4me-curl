c1eeca2817ff601af83eba7f0254bea7a1d359e3
Amazos <olegi_g@yahoo.com>
2016-08-03 14:24:57 +0400

    add new items to COnsolidate ORders to Vehicle Capacities user case

---
+++ UseCase/ConsolidateOrders/get_optimization_single.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+SET url=https://www.route4me.com
+SET apikey=11111111111111111111111111111111
+SET optprobid=38D4E661BB49089B92C7722F86ED7181
+
+ECHO ON
+curl -o file1.txt -g -X GET -k "%URL%/api.v4/optimization_problem.php?api_key=%apikey%&optimization_problem_id=%optprobid%" 
+
+timeout /t 30
