71759f61648c42c34ca1c329f9bbc2920859ca11
Amazos <olegi_g@yahoo.com>
2016-08-04 18:56:31 +0400

    add new items to NewOrdersExistingCustomers use case

--- UseCase/NewOrdersExistingCustomers/get_optimization_single.bat
+++ UseCase/NewOrdersExistingCustomers/get_optimization_single.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
 SET url=https://www.route4me.com/api.v4/optimization_problem.php
-SET apikey=11111111111111111111111111111111
-SET optprobid=CE44FE1C4F46FACA59BE052489BE8642
+SET apikey=1D479A8464677D9FC054EB7D00B046D2
+SET optprobid=49D8041BC3EDBA422B68E49F13768664
 
 ECHO ON
 curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&optimization_problem_id=%optprobid%" 
--- UseCase/NewOrdersExistingCustomers/new_orders_existing_customer_optimization.bat
+++ UseCase/NewOrdersExistingCustomers/new_orders_existing_customer_optimization.bat
@@ -5,11 +5,11 @@
 ::See video tutorial here: http://support.route4me.com/route-planning-help.php?id=manual0:tutorial2:chapter2:subchapter1
 
 SET URL=https://www.route4me.com/api.v4/optimization_problem.php
-SET apikey=11111111111111111111111111111111
+SET apikey=1D479A8464677D9FC054EB7D00B046D2
 SET route_path_output=Points
 
 ECHO ON
 
-curl -o file1.txt -g -X POST -k -d "@single_depot_multiple_driver_notime_window_data.json" "%URL%?api_key=%apikey%&route_path_output=%route_path_output%" 
+curl -o file1.txt -g -X POST -k -d "@new_orders_existing_customer_optimization_data.json" "%URL%?api_key=%apikey%&route_path_output=%route_path_output%" 
 
 timeout /t 30
