e1a1da923034e49acc7241ab943e6dfe47301a3d
Amazos <olegi_g@yahoo.com>
2016-08-04 19:10:23 +0400

    update NewOrdersExistingCustomers use case items

--- UseCase/NewOrdersExistingCustomers/new_orders_existing_customer_optimization.bat
+++ UseCase/NewOrdersExistingCustomers/new_orders_existing_customer_optimization.bat
@@ -1,11 +1,10 @@
 @ECHO OFF
 :: Single Depot, Multiple Driver, No Time Window
-:: Please put real value in route_date
 
 ::See video tutorial here: http://support.route4me.com/route-planning-help.php?id=manual0:tutorial2:chapter2:subchapter1
 
 SET URL=https://www.route4me.com/api.v4/optimization_problem.php
-SET apikey=1D479A8464677D9FC054EB7D00B046D2
+SET apikey=11111111111111111111111111111111
 SET route_path_output=Points
 
 ECHO ON
