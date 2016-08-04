d19627c347c92ae1cce0d84584574476c68cfbb4
Amazos <olegi_g@yahoo.com>
2016-08-04 15:21:17 +0400

    added new items to ManyStopsFewRoutes use case

--- UseCase/ManyStopsInFewRoutes/get_optimization_single.bat
+++ UseCase/ManyStopsInFewRoutes/get_optimization_single.bat
@@ -2,7 +2,7 @@
 
 SET url=https://www.route4me.com/api.v4/optimization_problem.php
 SET apikey=11111111111111111111111111111111
-SET optprobid=38D4E661BB49089B92C7722F86ED7181
+SET optprobid=CE44FE1C4F46FACA59BE052489BE8642
 
 ECHO ON
 curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&optimization_problem_id=%optprobid%" 
