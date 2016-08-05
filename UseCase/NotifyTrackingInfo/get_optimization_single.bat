df8c9d08fb87e9884f41c3d008b8dd5dfd907008
Amazos <olegi_g@yahoo.com>
2016-08-05 17:37:59 +0400

    added new RESPONSE file

--- UseCase/NotifyTrackingInfo/get_optimization_single.bat
+++ UseCase/NotifyTrackingInfo/get_optimization_single.bat
@@ -2,7 +2,7 @@
 
 SET url=https://www.route4me.com/api.v4/optimization_problem.php
 SET apikey=11111111111111111111111111111111
-SET optprobid=69E1F364F7D2B78FBE9FFB37A0755CEB
+SET optprobid=95F13F2A24E5EEC72A3A8CB143E02CE9
 
 ECHO ON
 curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&optimization_problem_id=%optprobid%" 
