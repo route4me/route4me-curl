b7996764f95828b4cd2ee1a19fe414d5dd9e59b8
Amazos <olegi_g@yahoo.com>
2016-08-05 18:28:26 +0400

    added new items to NotifyTrackingINfo use case files group

--- UseCase/NotifyTrackingInfo/set_gps_position.bat
+++ UseCase/NotifyTrackingInfo/set_gps_position.bat
@@ -1,20 +1,22 @@
 @ECHO OFF
-:: route_id and member_id are not real values - put real values
+
 SET url=https://www.route4me.com/track/set.php
 SET apikey=11111111111111111111111111111111
 SET frm=XML
 SET membid=1
-SET route_id=114B01238180A4227FD187E128C056F5
+SET route_id=FFFF72BF6E06DC3C746673279BDAAE05
 SET course=70
 SET speed=60
-SET lat=55.6884868
-SET lng=12.5366426
+SET lat=41.003671512
+SET lng=-81.5984610468
 SET devtype=android_phone
 SET devguid="HK5454H0K454564WWER445"
+SET vehid=FFFF72BF6E06DC3C746673279BDAAE05
 
 ECHO ON
 
-:: There are also several optional parameters: tx_id, vehicle_id altitude, device_timestamp, app_version
-curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&format=%frm%&member_id=%membid%&route_id=%route_id%&course=%course%&speed=%speed%&lat=%lat%&lng=%lng%&device_type=%devtype%&device_guid=%devguid%"
+:: There are also several optional parameters: tx_id, altitude, device_timestamp, app_version
+
+curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&format=%frm%&member_id=%membid%&route_id=%route_id%&course=%course%&speed=%speed%&lat=%lat%&lng=%lng%&device_type=%devtype%&device_guid=%devguid%&vehicle_id=%vehid%"
 
 timeout /t 30
