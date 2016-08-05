38aadd6c125d9914377954166db3da68937c9ccf
Amazos <olegi_g@yahoo.com>
2016-08-05 12:39:00 +0400

    added new items to ReceiveNotificationsAddressCheckin use case

--- UseCase/ReceiveNotificationAddressCheckin/get_optimization_single.bat
+++ UseCase/ReceiveNotificationAddressCheckin/get_optimization_single.bat
@@ -2,7 +2,7 @@
 
 SET url=https://www.route4me.com/api.v4/optimization_problem.php
 SET apikey=11111111111111111111111111111111
-SET optprobid=49D8041BC3EDBA422B68E49F13768664
+SET optprobid=69E1F364F7D2B78FBE9FFB37A0755CEB
 
 ECHO ON
 curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&optimization_problem_id=%optprobid%" 
