32c11cbfba7b81d9991b9ab1f2bd807527d528d6
Amazos <olegi_g@yahoo.com>
2016-08-29 18:12:00 +0400

    updated CreateDriversFleet use case files

--- UseCase/CreateDriversFleet/new_account_registration.bat
+++ UseCase/CreateDriversFleet/new_account_registration.bat
@@ -2,12 +2,12 @@
 
 SET url=https://www.route4me.com/actions/register_action.php
 SET apikey=11111111111111111111111111111111
-SET plan=enterprise_plan
+SET plan=free
 
 :: The example refers to a registration new Route4Me account.
 
 ECHO ON
 
-curl -vvv -k -X POST -H "Content-Type: multipart/form-data;" -F "strIndustry=Gifting" -F "strFirstName=Olman" -F "strLastName=Oland" -F "strEmail=ololol@outlook.com" -F "format=json" -F "chkTerms=1" -F "device_type=web" -F "strPassword_1=111111" -F "strPassword_2=111111" "%url%?plan=%plan%"
+curl -k -X POST -H "Content-Type: multipart/form-data;" -F "strIndustry=Gifting" -F "strFirstName=Olman" -F "strLastName=Oland" -F "strEmail=thewelco@gmail.com" -F "format=json" -F "chkTerms=1" -F "device_type=web" -F "strPassword_1=111111" -F "strPassword_2=111111" "%url%?plan=%plan%&member_type=5"
 
 timeout /t 30
