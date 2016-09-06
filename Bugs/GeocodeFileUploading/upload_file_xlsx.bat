726300673ec084af7e1cdb68783d3c0ff7f224ef
Amazos <olegi_g@yahoo.com>
2016-09-06 15:14:53 +0400

    add geocoding bug folder

---
+++ Bugs/GeocodeFileUploading/upload_file_xlsx.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+SET url=https://www.route4me.com/actions/upload/upload.php
+SET apikey=11111111111111111111111111111111
+SET "filename=address_book_sample.xlsx"
+
+ECHO ON
+
+curl -o file1.txt -g -k -X POST -F "strFilename=@%filename%" "%URL%?api_key=%apikey%&format=json" 
+
+timeout /t 30
