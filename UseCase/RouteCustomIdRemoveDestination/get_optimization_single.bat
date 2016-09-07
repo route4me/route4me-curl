879ffe655ff40d130484fee5d944e14105496290
Amazos <olegi_g@yahoo.com>
2016-09-07 15:15:55 +0400

    added files to RouteCustomIdRemoveDestination use case

--- UseCase/RouteCustomIdRemoveDestination/get_optimization_single.bat
+++ UseCase/RouteCustomIdRemoveDestination/get_optimization_single.bat
@@ -2,7 +2,7 @@
 
 SET url=http://www.route4me.com/api.v4/optimization_problem.php
 SET apikey=11111111111111111111111111111111
-SET optprobid=06BF0EFE40FCCC24957686DFE2DE353A
+SET optprobid=8E23ED7EEC79D27E5B2F092023FF055E
 
 ECHO ON
 curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&optimization_problem_id=%optprobid%" 
