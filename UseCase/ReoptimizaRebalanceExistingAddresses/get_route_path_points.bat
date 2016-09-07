c7ae8fe36897be458e5f88e60bb16cba73a1bb89
Amazos <olegi_g@yahoo.com>
2016-09-07 19:48:14 +0400

    add files to ReoptimizaRebalanceExistingAddresses use case

---
+++ UseCase/ReoptimizaRebalanceExistingAddresses/get_route_path_points.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+SET url=https://www.route4me.com/api.v4/route.php
+SET apikey=11111111111111111111111111111111
+SET routeid=F4CAF7AE81867B66FCD206AA23973DDA
+SET rpo=Points
+
+ECHO ON
+
+curl -o file1.txt -g -k -X GET "%url%?route_id=%routeid%&api_key=%apikey%&route_path_output=%rpo%"
+
+timeout /t 30
