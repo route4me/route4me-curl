0ccf28daf6b8e657b3ee40e6083656b078156975
Amazos <olegi_g@yahoo.com>
2016-09-09 12:56:28 +0400

    added files to InsertAddressIntoRoute use case

---
+++ UseCase/InsertAddressIntoRoute/add_addresses_to_route_optim_position.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+SET url=https://www.route4me.com/api.v4/route.php
+SET apikey=11111111111111111111111111111111
+SET routeid=5F9F4AE9FA82B3AFABB531E94EFAE19D
+
+ECHO ON
+
+curl -o file1.txt -k -g -X PUT -H "Content-Type: application/json" -d "@add_addresses_to_route_optim_position_data.json" "%url%?route_id=%routeid%&api_key=%apikey%"
+
+timeout /t 30
+
+
