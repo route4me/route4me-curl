881641433ce858a1a7709afea8a19055e908f3ec
Amazos <olegi_g@yahoo.com>
2016-09-10 18:32:25 +0400

    added files to CreateNewRouteFromOtherRoute use case

---
+++ UseCase/CreateNewRouteFromOtherRoute/duplicate_route.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+SET url=https://www.route4me.com/actions/duplicate_route.php
+SET apikey=11111111111111111111111111111111
+SET routeid=32587465EAA62D7412F9C37EF786C0AE
+
+ECHO ON
+curl -o file1.txt -g -k -X GET "%url%?api_key=%apikey%&route_id=%routeid%&to=none" 
+
+timeout /t 30
--- UseCase/CreateNewRouteFromOtherRoute/get_optimization_single.bat
+++ UseCase/CreateNewRouteFromOtherRoute/get_optimization_single.bat
@@ -2,7 +2,7 @@
 
 SET url=https://www.route4me.com/api.v4/optimization_problem.php
 SET apikey=11111111111111111111111111111111
-SET optprobid=DC389C49A9A1AE555336140E189BA104
+SET optprobid=4EF692E8FC5CD08B6E32DF19E50F3C1F
 
 ECHO ON
 curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&optimization_problem_id=%optprobid%" 
