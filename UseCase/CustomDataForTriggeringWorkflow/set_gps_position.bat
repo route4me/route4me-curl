16c283f0061b85ad45b98fec7d73e07346da58e0
Amazos <olegi_g@yahoo.com>
2016-09-20 16:33:06 +0400

    added files to CustomDataForTriggeringWorkflow use case

---
+++ UseCase/CustomDataForTriggeringWorkflow/set_gps_position.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+:: route_id and member_id are not real values - put real values
+SET url=https://www.route4me.com/track/set.php
+SET apikey=11111111111111111111111111111111
+SET frm=XML
+SET membid=1
+SET route_id=D456A17625782787E85FEFA06F206DAC
+SET course=280
+SET speed=60
+SET lat=41.194505989552
+SET lng=-81.443351581693
+SET devtype=android_phone
+SET devguid="HK5454H0K454564WWER445"
+
+ECHO ON
+
+:: There are also several optional parameters: tx_id, vehicle_id altitude, device_timestamp, app_version
+curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&format=%frm%&member_id=%membid%&route_id=%route_id%&course=%course%&speed=%speed%&lat=%lat%&lng=%lng%&device_type=%devtype%&device_guid=%devguid%"
+
+timeout /t 30
