19bfcfa1fcd71a4ae328182b506c9990b2ed4c1f
Amazos <olegi_g@yahoo.com>
2016-09-24 14:26:52 +0400

    added  files to ScanBarcodeFindRoute use case

---
+++ UseCase/ScanBarcodeFindRoute/update_route_destination_custom_data.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+SET url=https://www.route4me.com/api.v4/address.php
+
+:: You should insert real test data and your API key for accurate testing
+
+SET apikey=11111111111111111111111111111111
+SET routeid=723A57B984CDE04877C2E7EA8D1C84D9
+SET routedestinationid=187554385
+
+ECHO ON
+
+curl -o file1.txt -g -k -X PUT -H "Content-Type: application/json" -d "@update_route_destination_custom_data.json" "%url%?api_key=%apikey%&route_id=%routeid%&route_destination_id=%routedestinationid%"
+
+timeout /t 30
