75f5e5c3a9df6a0285a06d9864ac7077ebfbed8a
Amazos <olegi_g@yahoo.com>
2016-10-01 13:48:31 +0400

    updated add_addresses_to_route_RESPONSE.json file

--- Windows/Addresses/add_address_to_optimization.bat
+++ Windows/Addresses/add_address_to_optimization.bat
@@ -5,6 +5,6 @@ SET optprobid=07372F2CF3814EC6DFFAFE92E22771AA
 
 ECHO ON
 :: Worning - data in json file isn't real, you should put there real test data for right testing
-curl -o file1.txt -g -X PUT -k -d "@address_for_reoptimization.json" "%URL%/api.v4/optimization_problem.php?api_key=%apikey%&optimization_problem_id=%optprobid%&reoptimize=0" 
+curl -o file1.txt -g -X PUT -k -d "@address_for_optimization.json" "%URL%/api.v4/optimization_problem.php?api_key=%apikey%&optimization_problem_id=%optprobid%&reoptimize=0" 
 
 timeout /t 30
