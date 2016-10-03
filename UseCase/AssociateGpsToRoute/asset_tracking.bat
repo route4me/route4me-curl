8802d4dc94422f001519eb3931db1d524f38464f
Amazos <olegi_g@yahoo.com>
2016-10-03 20:21:23 +0400

    added files to AssociateGpsToRoute use case

--- UseCase/AssociateGpsToRoute/asset_tracking.bat
+++ UseCase/AssociateGpsToRoute/asset_tracking.bat
@@ -2,10 +2,10 @@
 :: asset_id is not real value in query parameter - put real value
 SET url=https://www.route4me.com/api/asset/find_route.php
 SET apikey=11111111111111111111111111111111
-SET query=2121541
+SET query=189877818
 
 ECHO ON
 
-curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&query=%query%"
+curl -o file1.txt -g -X GET -k "%url%?api_key=%apikey%&query=%query%"
 
 timeout /t 30
---
+++ UseCase/AssociateGpsToRoute/find_route.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+SET url=https://www.route4me.com/api.v4/route.php
+SET apikey=11111111111111111111111111111111
+SET query=Notebook
+
+ECHO ON
+curl -o file1.txt -g -k -X GET "%url%?api_key=%apikey%&query=%query%" 
+
+timeout /t 30
