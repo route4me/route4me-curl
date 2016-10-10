d2a909844b3c662cc04633c137a3c406ff9a1c2f
Amazos <olegi_g@yahoo.com>
2016-10-10 19:10:07 +0400

    updated member_create files

--- Windows/Members/member_create (v4).bat
+++ Windows/Members/member_create (v4).bat
@@ -1,24 +1,11 @@
 @ECHO OFF
 
 SET url=https://route4me.com/api.v4/user.php
-SET apikey=11111111111111111111111111111111
-SET email=skrynkovskyy2@gmail.com
+SET apikey=3E7F2F233DB726CF36E9610863E4727A
+SET email=olllandia654@gmail.com
 
 ECHO ON
 
-::curl -o file1.txt -g -H "Content-Type: application/json" -X POST -k -d"member_create_v4_data.json" "%url%?api_key=%apikey%"
-
-curl -o file1.txt -g -H "Content-Type: multipart/form-data;" -X POST -k ^
--F "member_first_name=Clay" ^
--F "member_last_name=Abraham" ^
--F "member_phone=571-259-5939" ^
--F "member_type=SUB_ACCOUNT_DISPATCHER" ^
--F "member_email=%email%" ^
--F "member_password=123456" ^
--F "OWNER_MEMBER_ID=1116" ^
--F "member_zipcode=22102" ^
--F "preferred_language=en" ^
--F "preferred_units=ml" ^
-"%url%?api_key=%apikey%"
+curl -o file1.txt -g -H "Content-Type: application/json" -X POST -k -d "@member_create_v4_data.json" "%url%?api_key=%apikey%"
 
 timeout /t 30
