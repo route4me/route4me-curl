2182f92597d556acce34869853fc69769a489450
Amazos <olegi_g@yahoo.com>
2016-10-12 15:07:10 +0400

    updated bash Territory files

--- Linux/Territory/update_circle_zone.sh
+++ Linux/Territory/update_circle_zone.sh
@@ -2,10 +2,9 @@
 
 url=https://www.route4me.com/api.v4/territory.php
 apikey=11111111111111111111111111111111
-terid1=F540DE7E2698FFA9217AE4C147D94259
-terid2=AE1B175E3AB12746125075DF8A088DC4
+terid=39236C3A30F92CA338C41EB0978F9D8A
 
-curl -o file1.txt -g -k -X PUT -d "@update_circle_zone_data.json" "$url?api_key=$apikey&territory_id=$terid1&territory_id=$terid2"
+curl -o file1.txt -g -k -X PUT -d "@update_circle_zone_data.json" "$url?api_key=$apikey&territory_id=$terid"
 
 echo "Finished..."
 
