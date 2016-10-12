ebbc9f6682daa2782f72a417e47970f77174e651
Amazos <olegi_g@yahoo.com>
2016-10-12 15:02:50 +0400

    updated cmd Territory files

--- Windows/Territory/create_rect_zone.bat
+++ Windows/Territory/create_rect_zone.bat
@@ -5,6 +5,6 @@ SET terid=03C0330958139E3EDF61EFFCEFBBD64E
 
 ECHO ON
 
-curl -o file1.txt -g -k -X POST -d '@create_rect_zone_data.json' "%url%?api_key=%apikey%&territory_id=%terid%"
+curl -o file1.txt -g -k -X POST -d "@create_rect_zone_data.json" "%url%?api_key=%apikey%&territory_id=%terid%"
 
 timeout /t 30
