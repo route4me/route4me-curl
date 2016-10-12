448379a5bfc7a5e65d30d13cebb2e3fe52077198
Amazos <olegi_g@yahoo.com>
2016-10-12 14:17:03 +0400

    updated curl(cmd) Avoidance Zone fies

--- Windows/Avoidance zones/update_circle_zone.bat
+++ Windows/Avoidance zones/update_circle_zone.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
+
 SET url=https://www.route4me.com/api.v4/avoidance.php
 SET apikey=11111111111111111111111111111111
-SET terid1=1AA62406364DF6C830FF5B6BCCB6B721
-SET terid2=30AB54EFCA2388E8D0C57D99B624DFAA
+SET terid=33C1D8EA0B3078B6238626259FBA914F
 
 ECHO ON
 
-curl -o file1.txt -g -k -X PUT -d '@update_circle_zone_data.json' "%url%?api_key=%apikey%&territory_id=%terid1%&territory_id=%terid2%"
+curl -o file1.txt -g -k -X PUT -d "@update_circle_zone_data.json" "%url%?api_key=%apikey%&territory_id=%terid%"
 
 timeout /t 30
