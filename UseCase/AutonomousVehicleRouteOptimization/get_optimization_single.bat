8f7d0d6004fc14082654568be834135cd649bf40
Amazos <olegi_g@yahoo.com>
2016-10-13 13:02:06 +0400

    updated AutonomousVehicleRouteOptimization use case

--- UseCase/AutonomousVehicleRouteOptimization/get_optimization_single.bat
+++ UseCase/AutonomousVehicleRouteOptimization/get_optimization_single.bat
@@ -2,7 +2,7 @@
 
 SET url=https://www.route4me.com/api.v4/optimization_problem.php
 SET apikey=11111111111111111111111111111111
-SET optprobid=3675E09901C899699D0A71B7BB6B6DFF
+SET optprobid=7D9063A61C489608D37DBB66633F67DD
 
 ECHO ON
 curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&optimization_problem_id=%optprobid%" 
