fc3569dce5a077f02f39189f89fef0dc8946a538
Amazos <olegi_g@yahoo.com>
2016-10-19 16:25:26 +0400

    added get_route_team_activities examples

---
+++ Windows/Activities/get_route_team_activities.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+:: You should insert real test data and your API key for accurate testing
+
+SET url=https://www.route4me.com/api/get_activities.php
+SET apikey=11111111111111111111111111111111
+SET route_id=06B655F27E0D6A74BD37F6F9758E4D2E
+SET team=true
+
+:: Example refers to the process of getting team activities on a route
+
+ECHO ON
+curl -o file1.txt -g -k -X GET "%URL%?api_key=%apikey%&route_id=%route_id%&team=%team%"
+
+timeout /t 30
