5aabefbe65ac4d632ed21769e6065f38528bc9a5
Amazos <olegi_g@yahoo.com>
2016-10-27 18:11:48 +0400

    updated asset_tracking example, added response

--- Windows/Tracking/asset_tracking.bat
+++ Windows/Tracking/asset_tracking.bat
@@ -1,11 +1,12 @@
 @ECHO OFF
-:: asset_id is not real value in query parameter - put real value
-SET url=https://www.route4me.com/api/asset/find_route.php
+
+SET url=https://route4me.com/api.v4/status.php
+
 SET apikey=11111111111111111111111111111111
-SET query=2121541
+SET tracking="Q7G9P1L9"
 
 ECHO ON
 
-curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&query=%query%"
+curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&tracking=%tracking%"
 
 timeout /t 30
