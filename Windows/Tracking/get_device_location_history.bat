f77987dcd9b8fd016b6b85b2a3793f828a27b5c7
Amazos <olegi_g@yahoo.com>
2016-10-27 17:20:57 +0400

    added get_device_location_history example

---
+++ Windows/Tracking/get_device_location_history.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+SET url=https://www.route4me.com/api/track/get_device_location.php
+SET apikey=11111111111111111111111111111111
+
+SET routeid=814FB49CEA8188D134E9D4D4B8B0DAF7
+SET time_period=all_time
+
+:: See other parameters at: https://github.com/route4me/route4me-json-schemas/blob/master/TrackingHistory_request.dtd
+
+ECHO ON
+
+curl -o file1.txt -k -g -X GET "%URL%?route_id=%routeid%&api_key=%apikey%&format=json&time_period=%time_period%" 
+
+timeout /t 30
