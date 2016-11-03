3ed97d68ab2e4215776c78b06294e11faeb7a750
Amazos <olegi_g@yahoo.com>
2016-11-03 19:36:58 +0400

    updated get_optimization_simulation.bat file

--- Windows/Depot API/get_optimization_simulation.bat
+++ Windows/Depot API/get_optimization_simulation.bat
@@ -6,7 +6,7 @@ SET job_id=6bcd0373-c72c-42f1-81d7-a36c48983e16
 
 ECHO ON
 
-:: Examlple refers to the process of getting of optimization simulation
+:: Examlple refers to the process of getting an optimization simulation (job)
 
 curl -o file1.txt -k -g -X GET "%url%%job_id%" 
 
