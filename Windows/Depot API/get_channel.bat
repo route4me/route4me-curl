83b0ed9233ae1fb3c02f7719707cd5d9d30c7286
Amazos <olegi_g@yahoo.com>
2016-11-03 19:31:00 +0400

    updated get_channel.bat file

--- Windows/Depot API/get_channel.bat
+++ Windows/Depot API/get_channel.bat
@@ -2,11 +2,10 @@
 SET url=https://apps.route4me.com/depots/api/v0.1/channel/
 
 SET apikey=11111111111111111111111111111111
-SET job_id=6bcd0373-c72c-42f1-81d7-a36c48983e16
 
 ECHO ON
 
-:: Examlple refers to the process of getting of optimization simulation
+:: Examlple refers to the process of getting a channel
 
 curl -o file1.txt -k -g -X GET "%url%" 
 
