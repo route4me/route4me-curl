9817ac95ee544776ad1eb148f486fcd5d1e499d5
Amazos <olegi_g@yahoo.com>
2016-11-03 18:55:35 +0400

    updated job_delete

--- Windows/Depot API/job_delete.bat
+++ Windows/Depot API/job_delete.bat
@@ -2,12 +2,12 @@
 SET url=https://apps.route4me.com/depots/api/v0.1/jobs/
 
 SET apikey=11111111111111111111111111111111
-SET pk=29
+SET pk=31
 
 ECHO ON
 
-:: Examlple refers to the process of getting of optimization simulation
+:: Examlple refers to the process of deleting a job
 
-curl -o file1.txt -k -g -X DELETE "%url%%pk%"
+curl -o file1.txt -k -g -X DELETE "%url%%pk%?api_key=%apikey%"
 
 timeout /t 30
