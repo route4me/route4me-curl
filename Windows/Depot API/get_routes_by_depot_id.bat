8093b3017317b9ab650a9d50f435ff5c371d1098
Amazos <olegi_g@yahoo.com>
2016-11-03 20:11:26 +0400

    added get_routes_by_depot_id example

--- Windows/Depot API/get_routes_by_depot_id.bat
+++ Windows/Depot API/get_routes_by_depot_id.bat
@@ -2,11 +2,11 @@
 SET url=https://apps.route4me.com/depots/api/v0.1/routes/
 
 SET apikey=11111111111111111111111111111111
-SET pk=28
+SET pk=192
 
 ECHO ON
 
-:: Examlple refers to the process of getting the routes by sending job ID
+:: Examlple refers to the process of getting the routes by sending depot ID
 
 curl -o file1.txt -k -g -X GET "%url%%pk%?api_key=%apikey%"
 
