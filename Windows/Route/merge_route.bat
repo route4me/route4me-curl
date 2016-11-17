eb942f0bd1604e23dc65cb222a8516a11af8d4f7
Amazos <olegi_g@yahoo.com>
2016-11-17 17:03:54 +0400

    updated merge_route.bat file

--- Windows/Route/merge_route.bat
+++ Windows/Route/merge_route.bat
@@ -1,8 +1,25 @@
 @ECHO OFF
-SET url=https://www.route4me.com
+
+setlocal enableDelayedExpansion
+
+SET url=https://www.route4me.com/actions/merge_routes.php
 SET apikey=11111111111111111111111111111111
+SET "route_ids=E819198B0D10364DD0AFC2944EAB3F17,F6BF7DC230A12D856DC78BBCB26F37D1"
+SET "depot_address=559 W Aurora Rd, Northfield, OH 44067"
+SET "depot_address=!depot_address: =%%20!"
+SET remove_origin=false
+SET depot_lat=41.315116882324
+SET depot_lng=-81.558746337891
 
 ECHO ON
-curl -o file1.txt -g -k -X POST -d '@merge_route_data.json' "%URL%/actions/merge_routes.php?api_key=%apikey%" 
+:: curl -o file1.txt -g -k -H "Content-Type: application/x-www-form-urlencoded" -X POST -d '@merge_route_data.json' "%url%?api_key=%apikey%"
+
+curl -o file1.txt -g -k -H "multipart/form-data" -X POST ^
+-F "route_ids=%route_ids%" ^
+-F "depot_address=%depot_address%" ^
+-F "remove_origin=%remove_origin%" ^
+-F "depot_lat=%depot_lat%" ^
+-F "depot_lng=%depot_lng%" ^
+"%url%?api_key=%apikey%"
 
 timeout /t 30
