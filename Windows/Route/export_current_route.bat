e917c5a19ed0609f4b518acc15de278301bebdfb
Amazos <olegi_g@yahoo.com>
2016-11-24 19:55:39 +0400

    added export_current_route example

---
+++ Windows/Route/export_current_route.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+SET url=https://www.route4me.com/actions/route/export_current_route.php
+SET apikey=1d479a8464677d9fc054eb7d00b046d2
+SET format=ml
+SET route_id=6F285696DE23B1A7C884BAAC57A1A471
+
+ECHO ON
+
+curl -o exported_route.kml -g -k -X POST "%url%?api_key=%apikey%&format=%format%&route_id=%route_id%"
+
+timeout /t 30
