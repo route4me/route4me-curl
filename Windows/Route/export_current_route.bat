92088b4b384debd96c9036c0c17e136dc3985b2d
Amazos <olegi_g@yahoo.com>
2016-11-25 10:42:26 +0400

    updated exported_route example

--- Windows/Route/export_current_route.bat
+++ Windows/Route/export_current_route.bat
@@ -2,7 +2,7 @@
 
 SET url=https://www.route4me.com/actions/route/export_current_route.php
 SET apikey=1d479a8464677d9fc054eb7d00b046d2
-SET format=ml
+SET format=kml
 SET route_id=6F285696DE23B1A7C884BAAC57A1A471
 
 ECHO ON
