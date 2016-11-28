024577a69d8235181adc14470cb48eeec7c08178
Amazos <olegi_g@yahoo.com>
2016-11-28 19:08:33 +0400

    updated member_edit example

--- Windows/Members/member_edit.bat
+++ Windows/Members/member_edit.bat
@@ -2,12 +2,11 @@
 
 SET url=https://www.route4me.com/api.v4/user.php
 SET apikey=11111111111111111111111111111111
-SET member_id=220461
 
-:: View existing sub-users in a Member's account
+:: Edit existing sub-users in a Member's account
 
 ECHO ON
 
-curl -o file1.txt -g -X PUT -d "@member_edit_data.json" -k "%url%?api_key=%apikey%&member_id=%member_id%"
+curl -o file1.txt -g -X PUT -d "@member_edit_data.json" -k "%url%?api_key=%apikey%"
 
 timeout /t 30
