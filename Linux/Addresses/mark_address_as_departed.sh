4ef55f5c1e4f09b71f67f0bc8fa81cd5e07c2ec7
Amazos <olegi_g@yahoo.com>
2016-11-29 19:23:37 +0400

    updated mark_address_as_departed examples

--- Linux/Addresses/mark_address_as_departed.sh
+++ Linux/Addresses/mark_address_as_departed.sh
@@ -7,7 +7,8 @@ addressid=160940135
 departed=1
 member=1
 
-# is_departed=1 means address was departed, is_departed=0 --- not departed
+# The example refers to the process of marking an address as departed.
+# is_departed=1 means address was marked as departed.
 
 curl -o file1.txt -g -k -X GET "$url?api_key=$apikey&route_id=$routeid&address_id=$addressid&is_departed=$departed=$departed&member_id=$member"
 
--- Windows/Addresses/mark_address_as_departed.bat
+++ Windows/Addresses/mark_address_as_departed.bat
@@ -2,14 +2,15 @@
 
 SET url=https://www.route4me.com/api/route/mark_address_departed.php
 SET apikey=11111111111111111111111111111111
-SET routeid=5C15E83A4BE005BCD1537955D28D51D7
-SET addressid=160940135
+SET routeid=DD376C7148E7FEE36CFABE2BD9978BDD
+SET addressid=183045808
 SET isdeparted=1
 SET member=1
 
 ECHO ON
 
-:: is_departed=1 means address was departed, is_departed=0 --- not departed
+:: The example refers to the process of marking an address as departed.
+:: is_departed=1 means address was marked as departed.
 
 curl -o file1.txt -g -k -X GET "%URL%?api_key=%apikey%&route_id=%routeid%&address_id=%addressid%&is_departed=%isdeparted%&member_id=%member%"
 
