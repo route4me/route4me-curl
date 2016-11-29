60e08a330932e8486f3415b1d4fdb90af961f4aa
Amazos <olegi_g@yahoo.com>
2016-11-29 19:43:39 +0400

    updated remove_destination_from_route examples

--- Linux/Addresses/remove_destination_from_route.sh
+++ Linux/Addresses/remove_destination_from_route.sh
@@ -5,6 +5,8 @@ apikey=11111111111111111111111111111111
 routeid=5C15E83A4BE005BCD1537955D28D51D7
 routedestinationid=160955262
 
+# The example refers to the process of removing an address from a route.
+
 curl -o file1.txt -g -X DELETE -k "$url?api_key=$apikey&route_id=$torouteid&route_destination_id=$routedestinationid"
 
 echo "Finished.."
--- Windows/Addresses/remove_destination_from_route.bat
+++ Windows/Addresses/remove_destination_from_route.bat
@@ -1,11 +1,14 @@
 @ECHO OFF
-SET url=https://www.route4me.com
+
+SET url=https://www.route4me.com/api.v4/address.php
 SET apikey=11111111111111111111111111111111
 SET routeid=5C15E83A4BE005BCD1537955D28D51D7
-SET routedestinationid=160955262
+SET routedestinationid=162891369
+
+:: The example refers to the process of removing an address from a route.
 
 ECHO ON
 
-curl -o file1.txt -g -X DELETE -k "%url%/api.v4/address.php?api_key=%apikey%&route_id=%torouteid%&route_destination_id=%routedestinationid%"
+curl -o file1.txt -g -X DELETE -k "%url%?api_key=%apikey%&route_id=%torouteid%&route_destination_id=%routedestinationid%"
 
 timeout /t 30
