5915954138d41a1068b54bbd18494e3a9ae00bf1
Amazos <olegi_g@yahoo.com>
2016-11-29 18:36:40 +0400

    updated Addressbook_get_contacts examples

--- Windows/Addressbook/Addressbook_get_contacts.bat
+++ Windows/Addressbook/Addressbook_get_contacts.bat
@@ -2,6 +2,8 @@ ECHO OFF
 SET url=http://www.route4me.com
 SET apikey=11111111111111111111111111111111
 
+:: The example refers to the process of getting limited number of the locations from an address book belonged to an user.
+
 ECHO ON
 
 curl -o file1.txt -X GET "%url%/api.v4/address_book.php?api_key=%apikey%&offset=0&limit=10"
