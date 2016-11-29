077720819f1a3c41b9a0bbb1bf9c61615f2403a5
Amazos <olegi_g@yahoo.com>
2016-11-29 19:02:19 +0400

    updated delete_destination_from_optimization examples

--- Linux/Addresses/delete_destination_from_optimization.sh
+++ Linux/Addresses/delete_destination_from_optimization.sh
@@ -5,6 +5,8 @@ apikey=11111111111111111111111111111111
 optprobid=123974C887534E84B6114F18A65847E3
 routedestinationid=154456307
 
+# The example refers to the process of removing an addres from an optimization.
+
 curl -o file1.txt -g -X DELETE -k "$url?api_key=$apikey&optimization_problem_id=$optprobid&route_destination_id=$routedestinationid"
 
 echo "Finished..."
--- Windows/Addresses/delete_destination_from_optimization.bat
+++ Windows/Addresses/delete_destination_from_optimization.bat
@@ -1,11 +1,14 @@
 @ECHO OFF
-SET url=https://www.route4me.com
+
+SET url=https://www.route4me.com/api.v4/address.php
 SET apikey=11111111111111111111111111111111
-SET optprobid=7EC3FC88737C29E93A54E88243ACBC77
-SET routedestinationid=163752152
+SET optprobid=692C7776603BB4858900E0D000CA102A
+SET routedestinationid=175374836
+
+:: The example refers to the process of removing an addres from an optimization.
 
 ECHO ON
 
-curl -o file1.txt -g -X DELETE -k "%url%/api.v4/address.php?api_key=%apikey%&optimization_problem_id=%optprobid%&route_destination_id=%routedestinationid%"
+curl -o file1.txt -g -X DELETE -k "%url%?api_key=%apikey%&optimization_problem_id=%optprobid%&route_destination_id=%routedestinationid%"
 
 timeout /t 30
