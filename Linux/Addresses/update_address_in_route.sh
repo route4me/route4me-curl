cd83e5fb6ce3dee7650e5867097ccde7e756b752
Amazos <olegi_g@yahoo.com>
2016-11-29 19:47:13 +0400

    updated update_address_in_route examples

--- Linux/Addresses/update_address_in_route.sh
+++ Linux/Addresses/update_address_in_route.sh
@@ -5,6 +5,7 @@ apikey=11111111111111111111111111111111
 routeid=5C15E83A4BE005BCD1537955D28D51D7
 routedestinationid=160940135
 
+# The example refers to the process of updating an address in a route.
 
 curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@address_update_data.json" "$url?api_key=$apikey&route_id=$routeid&route_destination_id=$routedestinationid"
 
