86015ab41272909e48cfcc81a294373de22bbf4e
Amazos <olegi_g@yahoo.com>
2016-11-29 20:01:40 +0400

    updated get_avoidance_zones examples

--- Linux/Avoidance zones/get_avoidance_zones.sh
+++ Linux/Avoidance zones/get_avoidance_zones.sh
@@ -3,6 +3,8 @@
 url=https://www.route4me.com/api.v4/avoidance.php
 apikey=11111111111111111111111111111111
 
+# The example refers to the process of getting all avoidance zones from an user account.
+
 curl -o file1.txt -g -k -X GET "$url?api_key=$apikey"
 
 echo "FInished..."
