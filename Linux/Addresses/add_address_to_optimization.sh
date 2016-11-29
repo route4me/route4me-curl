30046bbe005af359b02887199920f307e0259187
Amazos <olegi_g@yahoo.com>
2016-11-29 18:54:34 +0400

    updated add_address_to_optimization examples

--- Linux/Addresses/add_address_to_optimization.sh
+++ Linux/Addresses/add_address_to_optimization.sh
@@ -3,9 +3,10 @@
 url="https://www.route4me.com/api.v4/optimization_problem.php"
 apikey=11111111111111111111111111111111
 optprobid=07372F2CF3814EC6DFFAFE92E22771AA
-opt=0
+opt=1
 
-# Worning - data in json file isn't real, you should put there real test data for right testing
+# The example refers to the process of adding an addres to an optimization.
+# You should insert real test data and your API key for accurate testing.
 
 curl -o file1.txt -g -X PUT -k -d "@address_for_optimization.json" "$url?api_key=$apikey&optimization_problem_id=$optprobid&reoptimize=$opt" 
 
--- Windows/Addresses/add_address_to_optimization.bat
+++ Windows/Addresses/add_address_to_optimization.bat
@@ -1,10 +1,14 @@
 @ECHO OFF
-SET url=https://www.route4me.com
+
+SET url=https://www.route4me.com/api.v4/optimization_problem.php
 SET apikey=11111111111111111111111111111111
 SET optprobid=07372F2CF3814EC6DFFAFE92E22771AA
 
 ECHO ON
-:: Worning - data in json file isn't real, you should put there real test data for right testing
-curl -o file1.txt -g -X PUT -k -d "@address_for_optimization.json" "%URL%/api.v4/optimization_problem.php?api_key=%apikey%&optimization_problem_id=%optprobid%&reoptimize=1" 
+
+:: The example refers to the process of adding an addres to an optimization.
+:: You should insert real test data and your API key for accurate testing.
+
+curl -o file1.txt -g -X PUT -k -d "@address_for_optimization.json" "%url%?api_key=%apikey%&optimization_problem_id=%optprobid%&reoptimize=1" 
 
 timeout /t 30
