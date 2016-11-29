b99d21a37482a027798981720a070e7f4c49b400
Amazos <olegi_g@yahoo.com>
2016-11-29 18:48:06 +0400

    updated Addressbook_update_contact examples

--- Linux/Addressbook/Addressbook_update_contact.sh
+++ Linux/Addressbook/Addressbook_update_contact.sh
@@ -3,6 +3,7 @@
 url=http://www.route4me.com/api.v4/address_book.php
 apikey=11111111111111111111111111111111
 
+# The example refers to the process of updating existing location in an address book.
 
 curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@address_data_for_update.json" "$url?api_key=$apikey"
 
--- Windows/Addressbook/Addressbook_update_contact.bat
+++ Windows/Addressbook/Addressbook_update_contact.bat
@@ -1,9 +1,12 @@
 @ECHO OFF
-SET url=http://www.route4me.com
+
+SET url=http://www.route4me.com/api.v4/address_book.php
 SET apikey=11111111111111111111111111111111
+
+:: The example refers to the process of updating existing location in an address book.
 
 ECHO ON
 
-curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@address_data_for_update.json" "%url%/api.v4/address_book.php?api_key=%apikey%"
+curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@address_data_for_update.json" "%url%?api_key=%apikey%"
 
 timeout /t 30
