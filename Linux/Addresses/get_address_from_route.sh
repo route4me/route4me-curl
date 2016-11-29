ad2b3735d4f406eb2e4eb53e3b228f2239b783c3
Amazos <olegi_g@yahoo.com>
2016-11-29 19:16:09 +0400

    updated get_address_from_route examples

--- Linux/Addresses/get_address_from_route.sh
+++ Linux/Addresses/get_address_from_route.sh
@@ -5,6 +5,8 @@ apikey=11111111111111111111111111111111
 routeid=5C15E83A4BE005BCD1537955D28D51D7
 routedestinationid=160940135
 
+# The example refers to the process of getting an address from a route.
+
 curl -o file1.txt -g -X GET "$url?api_key=$apikey&route_id=$routeid&route_destination_id=$routedestinationid"
 
 echo "Finished..."
