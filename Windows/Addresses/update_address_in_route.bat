216dea9f60af29afc365b65eca6b02006f36687a
Amazos <olegi_g@yahoo.com>
2016-11-29 19:50:33 +0400

    updated update_address_in_route example

--- Windows/Addresses/update_address_in_route.bat
+++ Windows/Addresses/update_address_in_route.bat
@@ -1,8 +1,11 @@
 @ECHO OFF
+
 SET url=http://www.route4me.com/api.v4/address.php
 SET apikey=11111111111111111111111111111111
 SET routeid=5C15E83A4BE005BCD1537955D28D51D7
 SET routedestinationid=160940135
+
+:: The example refers to the process of updating an address in a route.
 
 ECHO ON
 
