cd88f7c35b436d4c659970a92fb8dda3f0d669d2
Amazos <olegi_g@yahoo.com>
2016-11-29 18:58:57 +0400

    updated add_addresses_to_route examples

--- Windows/Addresses/add_addresses_to_route.bat
+++ Windows/Addresses/add_addresses_to_route.bat
@@ -1,10 +1,14 @@
 @ECHO OFF
-SET url=http://www.route4me.com
+
+SET url=http://www.route4me.com/api.v4/route.php
 SET apikey=11111111111111111111111111111111
 SET routeid=26312274727B95E07BEC2711449C458A
 
+:: The example refers to the process of adding an addres to a route.
+:: You should insert real test data and your API key for accurate testing.
+
 ECHO ON
 
-curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@route_add_addresses_data.json" "%url%/api.v4/route.php?route_id=%routeid%&api_key=%apikey%"
+curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@route_add_addresses_data.json" "%url%?route_id=%routeid%&api_key=%apikey%"
 
 timeout /t 30
