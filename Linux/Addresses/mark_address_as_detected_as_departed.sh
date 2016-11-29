414103b36b33717a0681952586e64d6094701b8d
Amazos <olegi_g@yahoo.com>
2016-11-29 19:27:19 +0400

    updated mark_address_as_detected_as_departed examples

--- Linux/Addresses/mark_address_as_detected_as_departed.sh
+++ Linux/Addresses/mark_address_as_detected_as_departed.sh
@@ -5,7 +5,8 @@ apikey=11111111111111111111111111111111
 routeid=241466F15515D67D3F951E2DA38DE76D
 routedestinationid=167899269
 
-# is_departed=1 means address was departed, is_departed=0 --- not departed
+# The example refers to the process of marking an address as detected as departed.
+# is_departed=1 means address was departed.
 
 curl -o file1.txt -k -g -X PUT -H "Content-Type: application/json" -d "@mark_address_as_detected_as_departed_data.json" "$url?api_key=$apikey&route_id=$routeid&route_destination_id=$routedestinationid"
 
