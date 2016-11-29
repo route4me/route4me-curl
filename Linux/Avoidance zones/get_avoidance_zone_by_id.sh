eadafaf77e4ae4b621b9104ffc7d70d5ffce4f78
Amazos <olegi_g@yahoo.com>
2016-11-29 19:58:43 +0400

    updated get_avoidance_zone_by_id examples

--- Linux/Avoidance zones/get_avoidance_zone_by_id.sh
+++ Linux/Avoidance zones/get_avoidance_zone_by_id.sh
@@ -4,6 +4,8 @@ url=https://www.route4me.com/api.v4/avoidance.php
 apikey=11111111111111111111111111111111
 terid=02ACC3A8A552A5F2F0C09E6839DBB761
 
+# The example refers to the process of getting an avoidance zone by sending the avoidance zone ID.
+
 curl -o file1.txt -g -k -X GET "$url?api_key=$apikey&territory_id=$terid"
 
 echo "Finished..."
--- Windows/Avoidance zones/get_avoidance_zone_by_id.bat
+++ Windows/Avoidance zones/get_avoidance_zone_by_id.bat
@@ -1,7 +1,10 @@
 @ECHO OFF
+
 SET url=https://www.route4me.com/api.v4/avoidance.php
 SET apikey=11111111111111111111111111111111
 SET terid=02ACC3A8A552A5F2F0C09E6839DBB761
+
+:: The example refers to the process of getting an avoidance zone by sending the avoidance zone ID.
 
 ECHO ON
 curl -o file1.txt -g -k -X GET "%URL%?api_key=%apikey%&territory_id=%terid%"
