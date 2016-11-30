7aff39ca434f675e2ee00101c7fd1f11c3f9958c
Amazos <olegi_g@yahoo.com>
2016-11-30 14:42:00 +0400

    updated get_street_addresses_zip_all examples

--- Linux/Geocoding/get_street_addresses_zip_all.sh
+++ Linux/Geocoding/get_street_addresses_zip_all.sh
@@ -4,7 +4,7 @@ url=https://rapid.route4me.com/street_data/zipcode
 apikey=11111111111111111111111111111111
 zipcode=00601
 
-# get street data filtered by zipcode without limit
+# The example refers to the process of getting all street data filtered by specified zipcode.
 
 curl -o file1.txt -g -X GET -k "$url/$zipcode/?api_key=$apikey"
 
