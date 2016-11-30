089add9ef8ac84120db744a618722dc87b46304a
Amazos <olegi_g@yahoo.com>
2016-11-30 16:01:44 +0400

    updated member_create_v4 examples

--- Linux/Members/member_create_v4.sh
+++ Linux/Members/member_create_v4.sh
@@ -3,7 +3,7 @@
 url=https://www.route4me.com/api.v4/user.php
 apikey=11111111111111111111111111111111
 
-# Edit existing sub-users in a Member's account
+# The example refers to the process of creating a sub-user in a member's account
 
 curl -o file1.txt -g -H "Content-Type: application/json" -X POST -d "@member_create_v4_data.json" -k "$url?api_key=$apikey"
 
--- Windows/Members/member_create_v4.bat
+++ Windows/Members/member_create_v4.bat
@@ -3,6 +3,8 @@
 SET url=https://route4me.com/api.v4/user.php
 SET apikey=11111111111111111111111111111111
 
+:: The example refers to the process of creating a sub-user in a member's account
+
 ECHO ON
 
 curl -o file1.txt -g -H "Content-Type: application/json" -X POST -k -d "@member_create_v4_data.json" "%url%?api_key=%apikey%"
