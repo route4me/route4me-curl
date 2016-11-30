5a6801a4d231a50a9376d2dee7f770e767e3db40
Amazos <olegi_g@yahoo.com>
2016-11-30 18:28:37 +0400

    updated get_optimization_single examples

--- Linux/Optimizations/get_optimization_single.sh
+++ Linux/Optimizations/get_optimization_single.sh
@@ -4,6 +4,8 @@ url=https://www.route4me.com/api.v4/optimization_problem.php
 apikey=11111111111111111111111111111111
 optprobid=07372F2CF3814EC6DFFAFE92E22771AA
 
+# The example refers to the process of getting a single optimization problem.
+
 curl -o file1.txt -g -X GET -k "$url?api_key=$apikey&optimization_problem_id=$optprobid" 
 
 echo "Finished..."
--- Windows/Optimizations/get_optimization_single.bat
+++ Windows/Optimizations/get_optimization_single.bat
@@ -1,7 +1,10 @@
 @ECHO OFF
+
 SET url=http://www.route4me.com/api.v4/optimization_problem.php
-SET apikey=11111111111111111111111111111111
-SET optprobid=07372F2CF3814EC6DFFAFE92E22771AA
+SET apikey=59624B5A840F839ECB8537AADB58FF74
+SET optprobid=F685A2CC2466AFC4D2D04086897C01AD
+
+:: The example refers to the process of getting a single optimization problem.
 
 ECHO ON
 curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&optimization_problem_id=%optprobid%" 
