9c4547da19a40c0db7a2779e725f2c5f429e1dfb
Amazos <olegi_g@yahoo.com>
2016-11-30 17:24:43 +0400

    updated add_notes_to_route examples

--- Linux/Notes/add_notes_to_route.sh
+++ Linux/Notes/add_notes_to_route.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# route_id and address_id are not real - put real values
-
 url=https://www.route4me.com/actions/addRouteNotes.php
+
 apikey=11111111111111111111111111111111
-routeid=vf55g4fg5dfgfdg1fdgd21g2f
-addrid=787854545455
-dev_lat=33.132675170898
-dev_lng=-83.244743347168
+routeid=DD376C7148E7FEE36CFABE2BD9978BDD
+
+# The parameters of the address "2705 N River Rd, Stow, OH 44224"
+addrid=183045812
+dev_lat=41.145240783691
+dev_lng=-81.410247802734
+
 dev_type=web
 updatetype=dropoff
+content=Note example for Destination Audit Use Case
 
-# Always returns false - needs clarification
+# The example refers to the process of adding a note to a route by sending HTTP paramters.
 
-curl -o file1.txt -g -k -X POST -d "@add_notes_to_route_data.json" "$url?api_key=$apikey&route_id=$routeid&address_id=$addrid&dev_lat=$dev_lat&dev_lng=$dev_lng&device_type=$dev_type"
+curl -o file1.txt -g -k -X POST -H "Content-Type: multipart/form-data;" -F "strNoteContents=$content" -F "strUpdateType=$dropoff" "$url?api_key=$apikey&route_id=$routeid&address_id=$addrid&dev_lat=$dev_lat&dev_lng=$dev_lng&device_type=$dev_type"
 
 echo "Finished..."
 
--- Windows/Notes/add_notes_to_route.bat
+++ Windows/Notes/add_notes_to_route.bat
@@ -1,17 +1,23 @@
 @ECHO OFF
-:: route_id and address_id are not real - put real values
+
 SET url=https://www.route4me.com/actions/addRouteNotes.php
+
 SET apikey=11111111111111111111111111111111
-SET routeid=vf55g4fg5dfgfdg1fdgd21g2f
-SET addrid=787854545455
-SET dev_lat=33.132675170898
-SET dev_lng=-83.244743347168
+SET routeid=DD376C7148E7FEE36CFABE2BD9978BDD
+
+:: The parameters of the address "2705 N River Rd, Stow, OH 44224"
+SET addrid=183045812
+SET dev_lat=41.145240783691
+SET dev_lng=-81.410247802734
+
 SET dev_type=web
 SET updatetype=dropoff
+SET content=Note example for Destination Audit Use Case
+
+:: The example refers to the process of adding a note to a route by sending HTTP paramters.
 
 ECHO ON
 
-:: Always returns false - needs clarification
-curl -o file1.txt -g -k -X POST -d '@add_notes_to_route_data.json' "%url%?api_key=%apikey%&route_id=%routeid%&address_id=%addrid%&dev_lat=%dev_lat%&dev_lng=%dev_lng%&device_type=%dev_type%"
+curl -o file1.txt -g -k -X POST -H "Content-Type: multipart/form-data;" -F "strNoteContents=%content%" -F "strUpdateType=%dropoff%" "%url%?api_key=%apikey%&route_id=%routeid%&address_id=%addrid%&dev_lat=%dev_lat%&dev_lng=%dev_lng%&device_type=%dev_type%"
 
 timeout /t 30
