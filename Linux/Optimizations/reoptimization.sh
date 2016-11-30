09819a1cf3aad7dbb4b4ecb8112326f38e3a7fa6
Amazos <olegi_g@yahoo.com>
2016-11-30 18:43:58 +0400

    updated reoptimization examples

--- Linux/Optimizations/reoptimization.sh
+++ Linux/Optimizations/reoptimization.sh
@@ -4,6 +4,8 @@ url=https://www.route4me.com/api.v4/optimization_problem.php
 apikey=11111111111111111111111111111111
 optprobid=07372F2CF3814EC6DFFAFE92E22771AA
 
+# The example refers to the process of reoptimization an existing optimization.
+
 curl -o file1.txt -g -X PUT -k -d "@addresses_for_reoptimization.json" "$url?api_key=$apikey&optimization_problem_id=$optprobid&reoptimize=1" 
 
 echo "Finished.."
--- Windows/Optimizations/reoptimization.bat
+++ Windows/Optimizations/reoptimization.bat
@@ -1,9 +1,12 @@
 @ECHO OFF
-SET url=https://www.route4me.com
+
+SET url=https://www.route4me.com/api.v4/optimization_problem.php
 SET apikey=11111111111111111111111111111111
 SET optprobid=07372F2CF3814EC6DFFAFE92E22771AA
 
+:: The example refers to the process of reoptimization an existing optimization.
+
 ECHO ON
-curl -o file1.txt -g -X PUT -k -d "@addresses_for_reoptimization.json" "%URL%/api.v4/optimization_problem.php?api_key=%apikey%&optimization_problem_id=%optprobid%&reoptimize=1" 
+curl -o file1.txt -g -X PUT -k -d "@addresses_for_reoptimization.json" "%url%?api_key=%apikey%&optimization_problem_id=%optprobid%&reoptimize=1" 
 
 timeout /t 30
