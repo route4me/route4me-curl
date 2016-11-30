66ae99abd8b14693d7fb090e3f8b2755268306dc
Amazos <olegi_g@yahoo.com>
2016-11-30 12:35:53 +0400

    updated get_street_address_single examples

--- Linux/Geocoding/get_street_address_single.sh
+++ Linux/Geocoding/get_street_address_single.sh
@@ -4,7 +4,8 @@ url=https://rapid.route4me.com/street_data
 apikey=11111111111111111111111111111111
 pk=4
 
-# get single street data. Parameter pk is street sequential number in all streets list
+# The example refers to the process of getting single street data.
+# Parameter pk is street sequential number in all streets list.
 
 curl -o file1.txt -g -X GET -k "$url/$pk/?api_key=$apikey"
 
