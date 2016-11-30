4806ef3122cd2e1afe71d0dabb54c906cb551dc5
Amazos <olegi_g@yahoo.com>
2016-11-30 17:49:48 +0400

    updated get_address_notes examples

--- Linux/Notes/get_address_notes.sh
+++ Linux/Notes/get_address_notes.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# route_id and route_destination_id are not real - put real values
-
 url=https://www.route4me.com/api.v4/address.php
 apikey=11111111111111111111111111111111
 routeid=kllk645lkl6kllk6l5k6l5k6l5k6
 routedestinationid=545464355
+
+# The example refers to the process of getting the notes attached to an address of a route.
+# You should insert real test data and your API key for accurate testing
 
 curl -o file1.txt -g -k -X GET "$url?api_key=$apikey&route_id=$routeid&route_destination_id=$routedestinationid&notes=1"
 
--- Windows/Notes/get_address_notes.bat
+++ Windows/Notes/get_address_notes.bat
@@ -1,11 +1,14 @@
 @ECHO OFF
-:: route_id and route_destination_id are not real - put real values
+
 SET url=https://www.route4me.com/api.v4/address.php
 SET apikey=11111111111111111111111111111111
-SET routeid=kllk645lkl6kllk6l5k6l5k6l5k6
+SET routeid=5C15E83A4BE005BCD1537955D28D51D7
 SET routedestinationid=545464355
 
+:: The example refers to the process of getting the notes attached to an address of a route.
+:: You should insert real test data and your API key for accurate testing
+
 ECHO ON
-curl -o file1.txt -g -k -X GET "%URL%?api_key=%apikey%&route_id=%routeid%&route_destination_id=%routedestinationid%&notes=1"
+curl -o file1.txt -g -k -X GET "%url%?api_key=%apikey%&route_id=%routeid%&route_destination_id=%routedestinationid%&notes=1"
 
 timeout /t 30
