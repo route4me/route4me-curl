7febf20b9427615946d47436cb61ac54e484666b
Amazos <olegi_g@yahoo.com>
2016-11-30 12:39:08 +0400

    updated get_street_addresses_all examples

--- Linux/Geocoding/get_street_addresses_all.sh
+++ Linux/Geocoding/get_street_addresses_all.sh
@@ -3,8 +3,8 @@
 url=https://rapid.route4me.com/street_data/
 apikey=11111111111111111111111111111111
 
+# The example refers to the process of getting all street data.
 
-# get all street data
 curl -o file1.txt -g -X GET -k "$url?api_key=$apikey"
 
 echo "Finished..."
