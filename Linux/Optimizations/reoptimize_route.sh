49365f054a9c188f4e21b7533f55b5b0ab8ee388
Amazos <olegi_g@yahoo.com>
2016-11-30 18:49:05 +0400

    updated reoptimize_route examples

--- Linux/Optimizations/reoptimize_route.sh
+++ Linux/Optimizations/reoptimize_route.sh
@@ -5,7 +5,7 @@ apikey=11111111111111111111111111111111
 routeid=5C15E83A4BE005BCD1537955D28D51D7
 reoptimize=1
 
-# Example demonstrates the process of reoptimization existing route by sendinf of GET and POST data.
+# The example refers to the process of reoptimization an existing route.
 
 curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@reoptimize_route_data.json" "$url?route_id=$routeid&api_key=$apikey&reoptimize=$reoptimize"
 
--- Windows/Optimizations/reoptimize_route.bat
+++ Windows/Optimizations/reoptimize_route.bat
@@ -1,11 +1,14 @@
 @ECHO OFF
-SET url=http://www.route4me.com
+
+SET url=http://www.route4me.com/api.v4/route.php
 SET apikey=11111111111111111111111111111111
 SET routeid=5C15E83A4BE005BCD1537955D28D51D7
 SET reoptimize=1
 
+:: The example refers to the process of reoptimization an existing route.
+
 ECHO ON
 
-curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@reoptimize_route_data.json" "%url%/api.v4/route.php?route_id=%routeid%&api_key=%apikey%&reoptimize=%reoptimize%"
+curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@reoptimize_route_data.json" "%url%?route_id=%routeid%&api_key=%apikey%&reoptimize=%reoptimize%"
 
 timeout /t 30
