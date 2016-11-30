1eafdcb729cf2140e03902e2cfd5b0829d737b02
Amazos <olegi_g@yahoo.com>
2016-11-30 19:01:13 +0400

    updated get_order_by_date_inserted examples

--- Linux/Orders/get_order_by_date_inserted.sh
+++ Linux/Orders/get_order_by_date_inserted.sh
@@ -4,7 +4,7 @@ url=http://www.route4me.com/api.v4/order.php
 apikey="33383266363136302D623934312D3131"
 sdate="2016-06-15"
 
-# The example demonstrates the process of retirieving orders inserted on a specified date sent with HTTP GET
+# The example refers to the process of retrieving orders inserted on a specified date by sending HTTP GET data
 
 curl -o file1.txt -g -X GET "$url?api_key=$apikey&day_added_YYMMDD=$sdate"
 
--- Windows/Orders/get_order_by_date_inserted.bat
+++ Windows/Orders/get_order_by_date_inserted.bat
@@ -5,7 +5,7 @@ SET "sdate=2016-06-15"
 
 ECHO ON
 
-:: The example demonstrates the process of retirieving orders inserted on a specified date sent with HTTP GET
+:: The example refers to the process of retirieving orders inserted on a specified date sent with HTTP GET data
 
 curl -o file1.txt -g -X GET "%URL%?api_key=%apikey%&day_added_YYMMDD=%sdate%"
 
