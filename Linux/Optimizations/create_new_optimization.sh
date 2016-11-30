a012c0c87f63e1e00887e3e4f5a7182b522c9511
Amazos <olegi_g@yahoo.com>
2016-11-30 18:10:42 +0400

    updated create_new_optimization examples

--- Linux/Optimizations/create_new_optimization.sh
+++ Linux/Optimizations/create_new_optimization.sh
@@ -3,8 +3,11 @@
 url=https://www.route4me.com/api.v4/optimization_problem.php
 apikey=11111111111111111111111111111111
 
-curl -o file1.txt -g -k -X POST -d "@create_new_optimization_data.json"  "$url?api_key=$apikey" 
+curl -o file1.txt -g -k -X POST -d "@create_new_optimization_data.json"  "$url?api_key=$apikey"
+
+# The example refers to the process of creating new optimization by sending HTTP data.
 
 echo "Finished..."
 
 sleep 15
+
--- Windows/Optimizations/create_new_optimization.bat
+++ Windows/Optimizations/create_new_optimization.bat
@@ -3,7 +3,10 @@
 SET url=https://www.route4me.com/api.v4/optimization_problem.php
 SET apikey=11111111111111111111111111111111
 
+:: The example refers to the process of creating new optimization by sending HTTP data.
+
 ECHO ON
+
 curl -o file1.txt -g -k -X POST -d "@create_new_optimization_data.json"  "%url%?api_key=%apikey%" 
 
 timeout /t 30
