5d5fed266ec8ec72aa76fdfb2cf8e5f7f01d98ba
Amazos <olegi_g@yahoo.com>
2016-11-30 18:37:30 +0400

    updated get_optimizations examples

--- Linux/Optimizations/get_optimizations.sh
+++ Linux/Optimizations/get_optimizations.sh
@@ -6,6 +6,8 @@ state=1,2,3
 limit=10
 offset=0
 
+# The example refers to the process of getting the limited number of the optimization problems.
+
 curl -o file1.txt -g -X GET -k "$url?state=$state&api_key=$apikey&limit=$limit&offset=$offset" 
 
 echo "Finished..."
--- Windows/Optimizations/get_optimizations.bat
+++ Windows/Optimizations/get_optimizations.bat
@@ -1,8 +1,11 @@
 @ECHO OFF
-SET url=https://www.route4me.com
+
+SET url=https://www.route4me.com/api.v4/optimization_problem.php
 SET apikey=11111111111111111111111111111111
 
+:: The example refers to the process of getting the limited number of the optimization problems.
+
 ECHO ON
-curl -o file1.txt -g -X GET -k "%URL%/api.v4/optimization_problem.php?state=1,2,3&api_key=%apikey%&limit=10&offset=0" 
+curl -o file1.txt -g -X GET -k "%url%?state=1,2,3&api_key=%apikey%&limit=10&offset=0" 
 
 timeout /t 30
