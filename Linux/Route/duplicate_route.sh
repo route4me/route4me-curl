45cb68b5ba843f3dd5cac0b98845057e6c40b0b1
Amazos <olegi_g@yahoo.com>
2016-12-01 18:50:18 +0400

    updated duplicate_route examples

--- Linux/Route/duplicate_route.sh
+++ Linux/Route/duplicate_route.sh
@@ -5,6 +5,8 @@ apikey=11111111111111111111111111111111
 routeid=C963990B11B6E3BB0648C0195E683EF0
 to=none
 
+# The example refers to the process of making duplicate of a route.
+
 curl -o file1.txt -g -k -X GET "$url?api_key=$apikey&route_id=$routeid&to=$to"
 
 echo "Finished..."
--- Windows/Route/duplicate_route.bat
+++ Windows/Route/duplicate_route.bat
@@ -1,9 +1,12 @@
 @ECHO OFF
-SET url=https://www.route4me.com
+
+SET url=https://www.route4me.com/actions/duplicate_route.php
 SET apikey=11111111111111111111111111111111
 SET routeid=C963990B11B6E3BB0648C0195E683EF0
 
+:: The example refers to the process of making duplicate of a route.
+
 ECHO ON
-curl -o file1.txt -g -k -X GET "%URL%/actions/duplicate_route.php?api_key=%apikey%&route_id=%routeid%&to=none" 
+curl -o file1.txt -g -k -X GET "%url%?api_key=%apikey%&route_id=%routeid%&to=none" 
 
 timeout /t 30
