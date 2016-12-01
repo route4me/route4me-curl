5ac4ce594f9c94f671d1312ee5145f1a6156a64d
Amazos <olegi_g@yahoo.com>
2016-12-01 19:32:27 +0400

    updated insert_address_into_route_specific_destination examples

--- Linux/Route/insert_address_into_route_specific_destination.sh
+++ Linux/Route/insert_address_into_route_specific_destination.sh
@@ -2,10 +2,10 @@
 
 url=http://www.route4me.com/api.v4/route.php
 
-# You should insert real test data and your API key for accurate testing
-
 apikey=11111111111111111111111111111111
 routeid=5C15E83A4BE005BCD1537955D28D51D7
+
+# The example refers to the process of inserting an address into a route's specified position
 
 curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@insert_address_into_route_specific_destination_data.json" "$url?route_id=$routeid&api_key=$apikey"
 
--- Windows/Route/insert_address_into_route_specific_destination.bat
+++ Windows/Route/insert_address_into_route_specific_destination.bat
@@ -1,13 +1,14 @@
 @ECHO OFF
 
-SET url=http://www.route4me.com
+SET url=http://www.route4me.com/api.v4/route.php
 
-:: You should insert real test data and your API key for accurate testing
 SET apikey=11111111111111111111111111111111
 SET routeid=5C15E83A4BE005BCD1537955D28D51D7
 
+:: The example refers to the process of inserting an address into a route's specified position
+
 ECHO ON
 
-curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@insert_address_into_route_specific_destination_data.json" "%url%/api.v4/route.php?route_id=%routeid%&api_key=%apikey%"
+curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@insert_address_into_route_specific_destination_data.json" "%url%?route_id=%routeid%&api_key=%apikey%"
 
 timeout /t 30
