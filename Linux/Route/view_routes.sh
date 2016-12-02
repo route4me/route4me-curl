c2e54b13574491e3012b9ba04957ddb8ef0d4a5a
Amazos <olegi_g@yahoo.com>
2016-12-02 18:08:11 +0400

    updated view_routes examples

--- Linux/Route/view_routes.sh
+++ Linux/Route/view_routes.sh
@@ -5,6 +5,8 @@ apikey=11111111111111111111111111111111
 limit=20
 offset=100
 
+# The example refers to the process of getting limited number of routes from an user's account.
+
 curl -o file1.txt -g -X GET "$url?api_key=$apikey&limit=$limit&offset=$offset" 
 
 echo "Finished..."
--- Windows/Route/view_routes.bat
+++ Windows/Route/view_routes.bat
@@ -1,8 +1,11 @@
 @ECHO OFF
-SET url=http://www.route4me.com
+
+SET url=https://www.route4me.com/api.v4/route.php
 SET apikey=11111111111111111111111111111111
 
+:: The example refers to the process of getting limited number of routes from an user's account.
+
 ECHO ON
-curl -o file1.txt -g -X GET "%URL%/api.v4/route.php?api_key=%apikey%&limit=20&offset=100" 
+curl -o file1.txt -g -k -X GET "%url%?api_key=%apikey%&limit=20&offset=100" 
 
 timeout /t 30
