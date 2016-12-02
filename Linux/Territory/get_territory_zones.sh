e69648ef254bc7b7132066118d57d35d16410983
Amazos <olegi_g@yahoo.com>
2016-12-02 19:19:44 +0400

    updated get_territory_zones examples

--- Linux/Territory/get_territory_zones.sh
+++ Linux/Territory/get_territory_zones.sh
@@ -3,6 +3,8 @@
 url=https://www.route4me.com/api.v4/territory.php
 apikey=11111111111111111111111111111111
 
+# The example refers to the process of getting the territory zones from an user's account.
+
 curl -o file1.txt -g -k -X GET "$url?api_key=$apikey"
 
 echo "Finished..."
--- Windows/Territory/get_territory_zones.bat
+++ Windows/Territory/get_territory_zones.bat
@@ -1,6 +1,10 @@
 @ECHO OFF
+
 SET url=https://www.route4me.com/api.v4/territory.php
 SET apikey=11111111111111111111111111111111
+SET addresses=1
+
+:: The example refers to the process of getting the territory zones from an user's account.
 
 ECHO ON
 curl -o file1.txt -g -k -X GET "%URL%?api_key=%apikey%"
