49344f474ab283e8f4e818327a505ed18cc1c15a
Amazos <olegi_g@yahoo.com>
2016-12-02 16:22:32 +0400

    updated update_route examples

--- Windows/Route/update_route.bat
+++ Windows/Route/update_route.bat
@@ -1,10 +1,13 @@
 @ECHO OFF
-SET url=http://www.route4me.com
+
+SET url=http://www.route4me.com/api.v4/route.php
 SET apikey=11111111111111111111111111111111
 SET routeid=5C15E83A4BE005BCD1537955D28D51D7
 
 ECHO ON
 
-curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@update_route_data.json" "%url%/api.v4/route.php?route_id=%routeid%&api_key=%apikey%"
+:: The example refers to the process of updating a route by sending parameters with HTTP PUT method.
+
+curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@update_route_data.json" "%url%?route_id=%routeid%&api_key=%apikey%"
 
 timeout /t 30
