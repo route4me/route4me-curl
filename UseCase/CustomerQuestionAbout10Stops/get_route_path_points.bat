df8d38e56ce586aaf20cfa14c7691609f5bede20
Amazos <olegi_g@yahoo.com>
2016-12-02 12:55:28 +0400

    created CustomerQuestionAbout10Stops use case folder and files

---
+++ UseCase/CustomerQuestionAbout10Stops/get_route_path_points.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+SET url=https://www.route4me.com/api.v4/route.php
+SET apikey=11111111111111111111111111111111
+SET routeid=C181F52D5B8E8E91FEB28E227A6CA00C
+SET rpo=Points
+SET notes=1
+
+ECHO ON
+
+curl -o file1.txt -g -k -X GET "%url%?route_id=%routeid%&api_key=%apikey%&route_path_output=%rpo%&notes=%notes%"
+
+timeout /t 30
