2dd9724b0d48aba26ad0173e42455335e533713e
Amazos <olegi_g@yahoo.com>
2016-12-02 18:00:34 +0400

    updated update_route_destination_custom_data examples

--- Linux/Route/update_route_destination_custom_data.sh
+++ Linux/Route/update_route_destination_custom_data.sh
@@ -2,11 +2,11 @@
 
 url=http://www.route4me.com/api.v4/address.php
 
-# You should insert real test data and your API key for accurate testing
-
 apikey=11111111111111111111111111111111
 routeid=5C15E83A4BE005BCD1537955D28D51D7
 routedestinationid=160940135
+
+# The example refers to the process of updating a route by sending custom data of an address with HTTP PUT method.
 
 curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@update_route_destination_custom_data.json" "$url?api_key=$apikey&route_id=$routeid&route_destination_id=$routedestinationid"
 
--- Windows/Route/update_route_destination_custom_data.bat
+++ Windows/Route/update_route_destination_custom_data.bat
@@ -1,13 +1,15 @@
 @ECHO OFF
-SET url=http://www.route4me.com
 
-:: You should insert real test data and your API key for accurate testing
+SET url=https://www.route4me.com/api.v4/address.php
+
 SET apikey=11111111111111111111111111111111
-SET routeid=5C15E83A4BE005BCD1537955D28D51D7
-SET routedestinationid=160940135
+SET routeid=CA902292134DBC134EAF8363426BD247
+SET routedestinationid=174405640
+
+:: The example refers to the process of updating a route by sending custom data of an address with HTTP PUT method.
 
 ECHO ON
 
-curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@update_route_destination_custom_data.json" "%url%/api.v4/address.php?api_key=%apikey%&route_id=%routeid%&route_destination_id=%routedestinationid%"
+curl -o file1.txt -g -k -X PUT -H "Content-Type: application/json" -d "@update_route_destination_custom_data.json" "%url%?api_key=%apikey%&route_id=%routeid%&route_destination_id=%routedestinationid%"
 
 timeout /t 30
