13e57af2b5bb5c56a76c603f44c8c3fcf3152e8a
Amazos <olegi_g@yahoo.com>
2016-12-02 15:28:35 +0400

    updated move_destination_to_route examples

--- Linux/Route/move_destination_to_route.sh
+++ Linux/Route/move_destination_to_route.sh
@@ -3,14 +3,16 @@
 url=https://www.route4me.com/actions/route/move_route_destination.php
 apikey=11111111111111111111111111111111
 
-# address recipient route ID
+# Recipient route ID
 trid=BAD35EEE32BFF8E20C2077AC53761BC7
 
-# The destination to be moved - address "3524 WHEELER AVE, Louisville, KY, 40215" in the route "5BB7EA1E6E3E4633D1299505BDAEA953", sequence # = 8
+# The destination to be moved
 rdi=184611093
 
-# ID of the destination n recipient route, after which will be inserted moved destination. "2707 7TH ST, Louisville, KY, 40215"
+# The destination ID in a recipient route, after which will be inserted moving destination.
 adi=184611074
+
+# The example refers to the process of moving an address into a route after specified address.
 
 curl -o file1.txt -g -X POST -k -H "Content-Type: multipart/form-data;" -F "to_route_id=$trid" -F "route_destination_id=$rdi" -F "after_destination_id=$adi" "$url?api_key=$apikey"
 
