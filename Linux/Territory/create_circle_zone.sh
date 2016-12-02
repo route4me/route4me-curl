e3c48eb555695ccbfe45bc786ebf512c594add8d
Amazos <olegi_g@yahoo.com>
2016-12-02 18:36:33 +0400

    updated create_circle_zone examples

--- Linux/Territory/create_circle_zone.sh
+++ Linux/Territory/create_circle_zone.sh
@@ -5,6 +5,7 @@ apikey=11111111111111111111111111111111
 
 curl -o file1.txt -g -k -X POST -d "@create_circle_zone_data.json" "$url?api_key=$apikey"
 
+# The example refers to the process of creating a territory zone with circle shape by sending HTTP data.
 
 echo "Finished..."
 
