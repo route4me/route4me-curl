22bb168cb187097f0e48482a53b9470984820da3
Amazos <olegi_g@yahoo.com>
2016-12-06 15:10:56 +0400

    updated get_device_last_location_history examples

--- Linux/Tracking/get_device_last_location_history.sh
+++ Linux/Tracking/get_device_last_location_history.sh
@@ -2,9 +2,9 @@
 
 url=http://www.route4me.com/api.v4/route.php
 apikey=11111111111111111111111111111111
+routeid=32F63FD03B08A5754CF2D516198FC8BA
 
-# route_id is not real - put real value
-routeid=fgfg4f56g4fd54gfdgdfgf4654
+# The example refers to the process of getting the last location history of a GPS device.
 
 curl -o file1.txt -g -X GET "$url?route_id=$routeid&api_key=$apikey&device_tracking_history=1" 
 
--- Windows/Tracking/get_device_last_location_history.bat
+++ Windows/Tracking/get_device_last_location_history.bat
@@ -1,11 +1,13 @@
 @ECHO OFF
-SET url=http://www.route4me.com/api.v4/route.php
+
+SET url=https://www.route4me.com/api.v4/route.php
 SET apikey=11111111111111111111111111111111
-:: route_id is not real - put real value
-SET routeid=fgfg4f56g4fd54gfdgdfgf4654
+SET routeid=32F63FD03B08A5754CF2D516198FC8BA
+
+:: The example refers to the process of getting the last location history of a GPS device.
 
 ECHO ON
 
-curl -o file1.txt -g -X GET "%URL%?route_id=%routeid%&api_key=%apikey%&device_tracking_history=1" 
+curl -o file1.txt -k -g -X GET "%url%?route_id=%routeid%&api_key=%apikey%&device_tracking_history=1" 
 
 timeout /t 30
