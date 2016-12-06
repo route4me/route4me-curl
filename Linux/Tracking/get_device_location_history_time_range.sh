92d1c79e407a638defd0da263faf5f28fc5ea3dc
Amazos <olegi_g@yahoo.com>
2016-12-06 17:20:49 +0400

    updated get_device_location_history_time_range examples

---
+++ Linux/Tracking/get_device_location_history_time_range.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+url=https://www.route4me.com/api/track/get_device_location.php
+apikey=11111111111111111111111111111111
+routeid=814FB49CEA8188D134E9D4D4B8B0DAF7
+time_period=custom
+
+start=$(date -d"2016-10-20T00:00" +%s)
+end=$(date -d"2016-10-26T23:59" +%s)
+
+# The example refers to the process of getting last location history of a device during the specified time range by sending HTTP parameters.
+# See other parameters at: https://github.com/route4me/route4me-json-schemas/blob/master/TrackingHistory_request.dtd
+
+curl -o file1.txt -k -g -X GET "$url?route_id=$routeid&api_key=$apikey&format=json&time_period=$time_period&start_date=$start&end_date=$end"
+
+echo "Finished..."
+
+sleep 15
--- Windows/Tracking/get_device_location_history_time_range.bat
+++ Windows/Tracking/get_device_location_history_time_range.bat
@@ -10,11 +10,12 @@ call get_unix_time 2016-10-20_00:00:00 start
 
 call get_unix_time 2016-10-26_23:59:59 end
 
+:: The example refers to the process of getting last location history of a device during the specified time range by sending HTTP parameters.
 :: See other parameters at: https://github.com/route4me/route4me-json-schemas/blob/master/TrackingHistory_request.dtd
 
 ECHO ON
 
-curl -o file1.txt -k -g -X GET "%URL%?route_id=%routeid%&api_key=%apikey%&format=json&time_period=%time_period%&start_date=%start%&end_date=%end%"
+curl -o file1.txt -k -g -X GET "%url%?route_id=%routeid%&api_key=%apikey%&format=json&time_period=%time_period%&start_date=%start%&end_date=%end%"
 
 
 timeout /t 30
