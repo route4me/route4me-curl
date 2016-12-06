9170a242e631e7e7cfb74133d91b365c10965b27
Amazos <olegi_g@yahoo.com>
2016-12-06 15:03:31 +0400

    updated asset_tracking examples

--- Linux/Tracking/asset_tracking.sh
+++ Linux/Tracking/asset_tracking.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# asset_id is not real value in query parameter - put real value
-
-url=https://www.route4me.com/api/asset/find_route.php
+url=https://route4me.com/api.v4/status.php
 apikey=11111111111111111111111111111111
-query=2121541
+tracking="Q7G9P1L9"
 
-curl -o file1.txt -g -X GET -k "$url?api_key=$apikey&query=$query"
+# The example refers to the process of an asset tracking by sending HTTP parameters.
+
+curl -o file1.txt -g -X GET -k "$url?api_key=$apikey&tracking=$tracking"
 
 echo "Finished..."
 
--- Windows/Tracking/asset_tracking.bat
+++ Windows/Tracking/asset_tracking.bat
@@ -5,8 +5,10 @@ SET url=https://route4me.com/api.v4/status.php
 SET apikey=11111111111111111111111111111111
 SET tracking="Q7G9P1L9"
 
+:: The example refers to the process of an asset tracking by sending HTTP parameters.
+
 ECHO ON
 
-curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&tracking=%tracking%"
+curl -o file1.txt -g -X GET -k "%url%?api_key=%apikey%&tracking=%tracking%"
 
 timeout /t 30
