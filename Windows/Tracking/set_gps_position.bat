10cc4b656c5d9d0ca3e7c6b4d054721dd0f674bb
Amazos <olegi_g@yahoo.com>
2016-12-06 18:02:14 +0400

    updated set_gps_position examples

--- Windows/Tracking/set_gps_position.bat
+++ Windows/Tracking/set_gps_position.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-:: route_id and member_id are not real values - put real values
+
 SET url=https://www.route4me.com/track/set.php
 SET apikey=11111111111111111111111111111111
 SET frm=XML
@@ -14,7 +14,9 @@ SET devguid="HK5454H0K454564WWER445"
 
 ECHO ON
 
-:: There are also several optional parameters: tx_id, vehicle_id altitude, device_timestamp, app_version
-curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&format=%frm%&member_id=%membid%&route_id=%route_id%&course=%course%&speed=%speed%&lat=%lat%&lng=%lng%&device_type=%devtype%&device_guid=%devguid%"
+:: The example refers to the process of setting GPS position of a device by sending HTTP parameters.
+:: You can also use optional parameters: tx_id, vehicle_id altitude, device_timestamp, app_version.
+
+curl -o file1.txt -g -X GET -k "%url%?api_key=%apikey%&format=%frm%&member_id=%membid%&route_id=%route_id%&course=%course%&speed=%speed%&lat=%lat%&lng=%lng%&device_type=%devtype%&device_guid=%devguid%"
 
 timeout /t 30
