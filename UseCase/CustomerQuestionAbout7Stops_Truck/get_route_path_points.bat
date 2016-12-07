62162ea34517b0dc7c4da4b867f296086f9e09cf
Amazos <olegi_g@yahoo.com>
2016-12-07 14:46:11 +0400

    created CustomerQuestionAbout7Stops_Truck use case example folder and files

---
+++ UseCase/CustomerQuestionAbout7Stops_Truck/get_route_path_points.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+SET url=https://www.route4me.com/api.v4/route.php
+SET apikey=11111111111111111111111111111111
+SET routeid=C3FFCB38AF53270BF3F9F16FFD75C03B
+SET rpo=Points
+SET notes=1
+
+ECHO ON
+
+curl -o file1.txt -g -k -X GET "%url%?route_id=%routeid%&api_key=%apikey%&route_path_output=%rpo%&notes=%notes%"
+
+timeout /t 30
