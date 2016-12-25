0054312af9ad6b95b852e98ec5385e24f732181e
Amazos <olegi_g@yahoo.com>
2016-12-25 05:35:27 +0400

    created MemberConfiguration folder and files

---
+++ Linux/MemberConfiguration/delete_existing_config_key.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+url=https://route4me.com/api.v4/configuration-settings.php
+apikey=11111111111111111111111111111111
+
+# Example refers to the process of removing of a specified configuration key belonging to an account.
+
+curl -o file1.txt -k -g -X DELETE -H "Content-Type: application/json;" -d "@delete_existing_config_key_data.json" "$url?api_key=$apikey" 
+
+echo "Finished..."
+
+sleep 15
---
+++ Linux/MemberConfiguration/update_existing_config_key.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+url=https://route4me.com/api.v4/configuration-settings.php
+apikey=11111111111111111111111111111111
+
+# Example refers to the process of updating existing configuration key data.
+
+curl -o file1.txt -k -g -X PUT -H "Content-Type: application/json;" -d "@update_existing_config_key_data.json" "$url?api_key=$apikey"
+
+echo "Finished..."
+
+sleep 15
