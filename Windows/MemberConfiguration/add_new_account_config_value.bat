a1b97208d86169307ea6f5c16fcfc466cc102683
Amazos <olegi_g@yahoo.com>
2016-12-25 05:18:44 +0400

    updated add_new_account_config_value.bat file

--- Windows/MemberConfiguration/add_new_account_config_value.bat
+++ Windows/MemberConfiguration/add_new_account_config_value.bat
@@ -5,7 +5,7 @@ SET apikey=11111111111111111111111111111111
 
 ECHO ON
 
-:: Example refers to the process of new account creation.
+:: Example refers to the process of new account configuration key creation.
 
 curl -o file1.txt -k -g -X POST -H "Content-Type: application/json;" -d "@add_new_account_config_value_data2.json" "%url%?api_key=%apikey%" 
 
