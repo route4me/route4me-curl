fdb1cf2e262a624cfb45be84bda1f2721c6060bf
Amazos <olegi_g@yahoo.com>
2017-01-22 20:19:52 +0400

    updated duplicate_route.bat file

--- Windows/Route/duplicate_route.bat
+++ Windows/Route/duplicate_route.bat
@@ -7,6 +7,6 @@ SET routeid=C963990B11B6E3BB0648C0195E683EF0
 :: The example refers to the process of making duplicate of a route.
 
 ECHO ON
-curl -o file1.txt -g -k -X GET "%url%?api_key=%apikey%&route_id=%routeid%&to=none" 
+curl -o file1.txt -g -k "%url%?api_key=%apikey%&route_id=%routeid%&to=none" 
 
 timeout /t 30
