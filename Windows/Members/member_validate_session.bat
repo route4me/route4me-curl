6b60f6fb75084a7da51a555f4495cd89ecbe2d22
Amazos <olegi_g@yahoo.com>
2017-01-23 17:29:08 +0400

    updated member_validate_session.bat file

--- Windows/Members/member_validate_session.bat
+++ Windows/Members/member_validate_session.bat
@@ -1,14 +1,14 @@
 @ECHO OFF
 
 SET url=https://www.route4me.com/datafeed/session/validate_session.php
-SET apikey=1D479A8464677D9FC054EB7D00B046D2
-SET sesid=ea250d5b4d48735419d300a95add8208
-SET memid=44143
+SET apikey=11111111111111111111111111111111
+SET sesid=0e4c2aa391b9f130ad8e1ea56fb3fb0c
+SET memid=1
 
 :: The example refers to the process of validating an user's working session in the Route4Me application.
 
 ECHO ON
 
-curl -vvv -o file2.txt -g -X GET -k "%url%?api_key=%apikey%&session_guid=%sesid%&member_id=%memid%&format=json"
+curl -o file2.txt -g -X GET -k "%url%?api_key=%apikey%&session_guid=%sesid%&member_id=%memid%&format=json"
 
 timeout /t 30
