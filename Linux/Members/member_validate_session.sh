59637621c715898b311ca66cd8cf2119e485b79e
Amazos <olegi_g@yahoo.com>
2017-01-23 17:33:01 +0400

    updated member_validate_session.sh file

--- Linux/Members/member_validate_session.sh
+++ Linux/Members/member_validate_session.sh
@@ -8,7 +8,7 @@ frmt=json
 
 # The example refers to the process of validating an user's working session in the Route4Me application.
 
-curl -o file1.txt -g -X GET -k "$url?api_key=$apikey&session_guid=$sesid&member_id=$memid&$frmt=json"
+curl -o file1.txt -g -X GET -k "$url?api_key=$apikey&session_guid=$sesid&member_id=$memid&format=$frmt"
 
 echo "Finished..."
 
