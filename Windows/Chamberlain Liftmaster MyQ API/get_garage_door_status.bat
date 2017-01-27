208cd94f44eb78c23d8081dced6650d51c824561
Amazos <olegi_g@yahoo.com>
2017-01-27 14:23:57 +0400

    updatedChamberlain Liftmaster MyQ API roject

--- Windows/Chamberlain Liftmaster MyQ API/get_garage_door_status.bat
+++ Windows/Chamberlain Liftmaster MyQ API/get_garage_door_status.bat
@@ -1,9 +1,11 @@
 @ECHO OFF
 
+SET "json_file=get_devices_and_info_about_myq_system_RESPONSE.json"
+
 :: The example refers to the process of getting garage dor status
 :: device_id (garage opener id) and filter conditions are defined in the file jq_filter.txt
 :: The equivalent of the command 'type' in the Linux is 'cat'
-:: jq-win64.exe file is windows JSON parser file. There are version for other platfroms: jq-linux64, jq-osx-amd64
+:: jq-win64.exe file is windows JSON parser file. There are versions for other platfroms: jq-linux64, jq-osx-amd64
 
 ECHO ON
 
--- Windows/Chamberlain Liftmaster MyQ API/myq_authentication.bat
+++ Windows/Chamberlain Liftmaster MyQ API/myq_authentication.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 :: Example refers to the process of authentication on the MyQ service
-:: You should only need to store the securityToken in the response for subsequent requests.
+:: You need to remember the securityToken in the response for subsequent requests.
 :: After the certain time the established connection can be broken, in this case you have to login again.
 :: In order to obtain MyQ account, you have to power MyQ device and to pair your mobile device (or PC) with MyQ device (see details at the link: https://setup.myqdevice.com/)
 
