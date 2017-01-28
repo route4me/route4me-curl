8690458b8d43163a32912ae763c274b28693de79
Amazos <olegi_g@yahoo.com>
2017-01-28 23:26:24 +0400

    created folder garage_door_opener and example files

---
+++ UseCase/AutonomousVehicleRouteOptimization/garage_door_opener/get_garage_door_status.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+SET "json_file=get_devices_and_info_about_myq_system_RESPONSE.json"
+
+:: The example refers to the process of getting garage dor status
+:: device_id (garage opener id) and filter conditions are defined in the file jq_filter.txt
+:: The equivalent of the command 'type' in the Linux is 'cat'
+:: jq-win64.exe file is windows JSON parser file. There are versions for other platfroms: jq-linux64, jq-osx-amd64
+
+ECHO ON
+
+type %json_file% | jq-win64 -f jq_filter.txt >file3.txt
+
+timeout /t 30
