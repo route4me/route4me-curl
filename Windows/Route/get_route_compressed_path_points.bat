e6aae0a8eb763eafd9e1b7b6e2162da36d2a6232
Amazos <olegi_g@yahoo.com>
2017-02-08 05:41:17 -0800

    created get_route_compressed_path_points example

---
+++ Windows/Route/get_route_compressed_path_points.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+SET url=http://www.route4me.com/api.v4/route.php
+SET apikey=11111111111111111111111111111111
+SET routeid=4728372005DE97EF9E4205852D690E34
+SET rpo=Points
+SET compress_path_points=true
+
+:: The example refers to the process of getting a route with the path points.
+
+ECHO ON
+
+curl -o file1.txt -g -X GET "%url%?route_id=%routeid%&api_key=%apikey%&route_path_output=%rpo%&compress_path_points=%compress_path_points%"
+
+timeout /t 30
