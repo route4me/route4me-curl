083f086b4287d75438a399f1c5de558794a43404
Amazos <olegi_g@yahoo.com>
2017-02-10 04:43:12 -0800

    updated file batch_geocoding_esc.bat

--- Windows/Geocoding/batch_geocoding_esc.bat
+++ Windows/Geocoding/batch_geocoding_esc.bat
@@ -7,8 +7,6 @@ SET apikey=11111111111111111111111111111111
 SET format=json
 SET "address=100 Pier 4 Boulevard, Boston, MA 02210, USA"
 
-::SET addrs="Los20%Angeles20%International20%Airport,20%CA"
-
 SET "address=!address: =%%20!"
 
 ECHO ON
