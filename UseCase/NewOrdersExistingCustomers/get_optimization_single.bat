986fc70c4cc64387bd3dcc1cf3863fbc5705a831
Amazos <olegi_g@yahoo.com>
2017-02-16 01:23:29 -0800

    updated NewOrdersExistingCustomers project scripts

--- UseCase/NewOrdersExistingCustomers/get_optimization_single.bat
+++ UseCase/NewOrdersExistingCustomers/get_optimization_single.bat
@@ -2,7 +2,7 @@
 
 SET url=https://www.route4me.com/api.v4/optimization_problem.php
 SET apikey=11111111111111111111111111111111
-SET optprobid=49D8041BC3EDBA422B68E49F13768664
+SET optprobid=4476413137906D5521F2CCEC28A157E2
 
 ECHO ON
 curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&optimization_problem_id=%optprobid%" 
