004edbfc7e8c9ef48cc083fef9ec753fdf76c0f1
Amazos <olegi_g@yahoo.com>
2017-02-22 17:14:39 +0400

    added to Hybrid Optimizations scheduled orders examples

--- Windows/Optimizations/Hybrid Data Optimization/reoptimization.bat
+++ Windows/Optimizations/Hybrid Data Optimization/reoptimization.bat
@@ -2,11 +2,11 @@
 
 SET url=https://www.route4me.com/api.v4/optimization_problem.php
 SET apikey=11111111111111111111111111111111
-SET optprobid=C1B821B94C5FFCE77001B3B2AA1B399C
+SET optprobid=C86B7F391E6A08187FEA0D623B48DA32
 
 :: The example refers to the process of reoptimization an existing optimization.
 
 ECHO ON
-curl -o file1.txt -g -X PUT -k -d "@reoptimization_data.json" "%url%?api_key=%apikey%&optimization_problem_id=%optprobid%&reoptimize=1" 
+curl -o file1.txt -g -X PUT -k "%url%?api_key=%apikey%&optimization_problem_id=%optprobid%&reoptimize=1" 
 
 timeout /t 30
