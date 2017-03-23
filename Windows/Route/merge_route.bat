1ed3cc5c2b25182d98eed8af9f700649b1665fbf
Amazos <olegi_g@yahoo.com>
2017-03-23 11:21:22 +0400

    updated merge_routes example

--- Windows/Route/merge_route.bat
+++ Windows/Route/merge_route.bat
@@ -3,20 +3,19 @@
 setlocal enableDelayedExpansion
 
 SET url=https://www.route4me.com/actions/merge_routes.php
-SET apikey=1D479A8464677D9FC054EB7D00B046D2
+SET apikey=11111111111111111111111111111111
 SET route_destination_id=167899059
-SET route_ids=56E8F6BF949670F0C0BBAC00590FD116,A6DAA07A7D4737723A9C85E7C3BA2351
-SET "depot_address=11921 N Dickinson Dr, Fredericksburg, VA 22407, USA"
+SET route_ids=807D38F914B27A7E9B05EDF0E1BE4014,F9165689AEB9818ED61AF3CC1C759AE4
+SET "depot_address=10180 Dyer St, El Paso, TX 79924, USA"
 SET "remove_origin=false"
-SET depot_lat=38.285804
-SET depot_lng=-77.555054
+SET depot_lat=31.9061405
+SET depot_lng=-106.4033899
 
 ECHO ON
 
 :: The example refers to the process of merging of the two routes into one route.
-:: Attention - this function requires clarification, it doesn't work properly. Will be updated soon.
 
-curl -o file1.txt -g -k -H "multipart/form-data" -X POST ^
+curl -o merge_routes_RESPONSE.json -g -k -H "multipart/form-data" -X POST ^
 -F "route_ids=%route_ids%" ^
 -F "depot_address=%depot_address%" ^
 -F "remove_origin=%remove_origin%" ^
