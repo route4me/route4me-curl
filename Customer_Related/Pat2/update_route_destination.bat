960f05ca9d2032ddf4268f4eb7d4435c757614f3
Amazos <olegi_g@yahoo.com>
2017-04-07 17:57:25 +0400

    added examples for customer

--- Customer_Related/Pat2/update_route_destination.bat
+++ Customer_Related/Pat2/update_route_destination.bat
@@ -2,14 +2,14 @@
 
 SET url=https://www.route4me.com/api.v4/address.php
 
-SET apikey=4dc953f3649388afacb498836476b05d
-SET routeid=23CA316B9CB34B2EB992E183A62EB57C
-SET routedestinationid=222432885,
+SET apikey=bd48828717021141485a701453273458
+SET routeid=5723944D461A50C8B98EA0416D6A3882
+SET routedestinationid=220386831,
 
 :: The example refers to the process of updating a route destination by sending custom data of an address with HTTP PUT method.
 
 ECHO ON
 
-curl -o update_destination_custom_RESPONSE.json -g -k -X PUT -H "Content-Type: application/json" -d "@update_route_destination_custom_data_RIGHT.json" "%url%?api_key=%apikey%&route_id=%routeid%&route_destination_id=%routedestinationid%"
+curl -o update_destination_custom_RESPONSE.json -g -k -X PUT -H "Content-Type: application/json" -d "@update_route_destination_custom_data_WRONG_2.json" "%url%?api_key=%apikey%&route_id=%routeid%&route_destination_id=%routedestinationid%"
 
 timeout /t 30
