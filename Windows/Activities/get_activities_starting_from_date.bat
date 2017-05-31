bd813c2ac8658b3d205ea97c4701c98031d5be4c
Amazos <olegi_g@yahoo.com>
2017-05-31 16:00:05 +0400

    updated get_activities_starting_from_date example

--- Windows/Activities/get_activities_starting_from_date.bat
+++ Windows/Activities/get_activities_starting_from_date.bat
@@ -3,7 +3,7 @@
 :: You should insert real test data and your API key for accurate testing
 
 SET url=https://www.route4me.com/api/get_activities.php
-SET apikey=CCF1E4E274174EDBAC434B37F6108C8E
+SET apikey=11111111111111111111111111111111
 SET offset=0
 SET limit=20
 SET start=1495238400
