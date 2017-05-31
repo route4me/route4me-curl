1f1db6f2c244e1e35631a6a591e60475d5b91aa7
Amazos <olegi_g@yahoo.com>
2017-05-31 12:07:53 +0400

    added get_activities_staring_from_date example (bash)

---
+++ Linux/Activities/get_activities_staring_from_date.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# You should insert real test data and your API key for accurate testing
+
+url=https://www.route4me.com/api/get_activities.php
+apikey=11111111111111111111111111111111
+offset=0
+limit=20
+start=1495238400
+
+curl -o file1.txt -g -k -X GET "$url?api_key=$apikey&offset=$offset&limit=$limit&start=$start"
+
+echo "Finished..."
+
+sleep 15
