b7fe1d0f8f900e070a0167577422910bdad91c22
Amazos <olegi_g@yahoo.com>
2017-06-11 21:03:45 +0400

    added recompute directions example (linux)

---
+++ Linux/Route/recompute_directions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+url=https://www.route4me.com/api.v4/route.php
+apikey=11111111111111111111111111111111
+routeid=79F570D5523C8156955786789DACDEC8
+
+# The example refers to the process of recomputing a route directions by sending parameters with HTTP PUT method.
+
+curl -o file1.txt -g -k -X PUT -H "Content-Type: application/json" -d "@recompute_directions_data.json" "$url?route_id=$routeid&api_key=$apikey&recompute_directions=1"
+
+echo "Finished..."
+
+sleep 15
--- Windows/Route/recompute_directions.bat
+++ Windows/Route/recompute_directions.bat
@@ -1,13 +1,13 @@
 @ECHO OFF
 
-SET url=http://www.route4me.com/api.v4/route.php
+SET url=https://www.route4me.com/api.v4/route.php
 SET apikey=11111111111111111111111111111111
 SET routeid=79F570D5523C8156955786789DACDEC8
 
 ECHO ON
 
-:: The example refers to the process of recomputing the route directions by sending parameters with HTTP PUT method.
+:: The example refers to the process of recomputing a route directions by sending parameters with HTTP PUT method.
 
-curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@recompute_directions_data.json" "%url%?route_id=%routeid%&api_key=%apikey%&recompute_directions=1"
+curl -o file1.txt -g -k -X PUT -H "Content-Type: application/json" -d "@recompute_directions_data.json" "%url%?route_id=%routeid%&api_key=%apikey%&recompute_directions=1"
 
 timeout /t 30
