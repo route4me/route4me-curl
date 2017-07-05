a565147a2fb7b9de1ca72fea2b3a65ee85a433cd
Amazos <olegi_g@yahoo.com>
2017-07-05 19:11:41 +0400

    updated batch_geocoding example

--- Windows/Geocoding/batch_geocoding.bat
+++ Windows/Geocoding/batch_geocoding.bat
@@ -6,14 +6,15 @@ SET apikey=11111111111111111111111111111111
 SET format=json
 
 :: You can specify many addresses, divided by /n, in one string.
+
 SET addrs="Los Angeles International Airport, CA\nn512,Florida Pl,Barberton,OH,44203\n3495,Purdue St,Cuyahoga Falls,OH,44221"
 
 SET "addrs=!addrs: =%%20!"
 
 ECHO ON
 
-:: The example refers to the process of the forward geocoding.
+:: The example refers to the process of the batch forward geocoding.
 
-curl -o file1.txt -k -g -X POST -F "strExportFormat=json" -F "addresses=%addrs%" "%url%?api_key=%apikey%" 
+curl -o batch_geocoding_RESPONSE.json -k -g -X POST -F "strExportFormat=json" -F "addresses=%addrs%" "%url%?api_key=%apikey%" 
 
 timeout /t 30
