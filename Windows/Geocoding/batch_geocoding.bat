941a10665919c323625ede5cb9daa053545248a7
Amazos <olegi_g@yahoo.com>
2017-07-05 16:05:49 +0400

    updated batch_geocoding.bat example (cmd)

--- Windows/Geocoding/batch_geocoding.bat
+++ Windows/Geocoding/batch_geocoding.bat
@@ -1,13 +1,19 @@
 @ECHO OFF
+setlocal enableDelayedExpansion
+
 SET url=https://www.route4me.com/api/geocoder.php
 SET apikey=11111111111111111111111111111111
 SET format=json
-SET addrs="Los20%Angeles20%International20%Airport,20%CA"
+
+:: You can specify many addresses, divided by /n, in one string.
+SET addrs="Los Angeles International Airport, CA\nn512,Florida Pl,Barberton,OH,44203\n3495,Purdue St,Cuyahoga Falls,OH,44221"
+
+SET "addrs=!addrs: =%%20!"
 
 ECHO ON
 
 :: The example refers to the process of the forward geocoding.
 
-curl -o file1.txt -k -g -X POST "%URL%?api_key=%apikey%&format=%format%&addresses=%addrs%" 
+curl -o file1.txt -k -g -X POST -F "strExportFormat=json" -F "addresses=%addrs%" "%url%?api_key=%apikey%" 
 
 timeout /t 30
