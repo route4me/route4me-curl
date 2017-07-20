123ffbf4e908c732d2fd135e8325850b03aa7fc3
Amazos <olegi_g@yahoo.com>
2017-07-20 11:28:31 +0400

    added get_route_compressed_path_points example

---
+++ Linux/Route/get_route_compressed_path_points.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+url=http://www.route4me.com/api.v4/route.php
+apikey=11111111111111111111111111111111
+routeid=4728372005DE97EF9E4205852D690E34
+rpo=Points
+compress_path_points=true
+
+# The example refers to the process of getting a route with the encoded & compressed path points.
+# For a path compreesion is used Encoded Polyline Algorithm Format (see link: https://developers.google.com/maps/documentation/utilities/polylinealgorithm)
+# There are interactive decoder polyline utilities in the Internet (see link: https://developers.google.com/maps/documentation/utilities/polylineutility)
+
+curl -o file1.txt -g -X POST -H "Content-Type: application/json" -d "@new_route_data.json" "$url?api_key=$apikey&route_id=$routeid&route_path_output=$rpo&compress_path_points=$compress_path_points"
+
+echo "Finished..."
+
+sleep 15
