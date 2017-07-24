5390cb5f08cd93d56169a060525145eb07786b95
Amazos <olegi_g@yahoo.com>
2017-07-24 20:01:12 +0400

    created Blog API folder and files

---
+++ Linux/Blog API/get_limited_ebooks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Example refers to the process of getting the ebooks.
+
+host=https://blog.route4me.com/wp-json/wp/v2/ebooks
+
+apikey=11111111111111111111111111111111
+page=1
+per_page=20
+context=embed
+
+curl -o "get_ebooks_embed_RESPONSE.json" -L -g -k -X GET "$host?api_key=$apikey&page=$page&per_page=$per_page&context=$context"
+curl -o file1.txt -g -k -X GET "$url?api_key=$apikey&offset=$offset&limit=$limit"
+
+echo "Finished..."
+
+sleep 15
--- Windows/Blog API/get_limited_ebooks.bat
+++ Windows/Blog API/get_limited_ebooks.bat
@@ -7,8 +7,9 @@ SET host=https://blog.route4me.com/wp-json/wp/v2/ebooks
 SET apikey=11111111111111111111111111111111
 SET page=1
 SET per_page=20
+SET context=embed
 
 ECHO ON
-curl -o "get_ebooks_embed_RESPONSE.json" -L -g -k -X GET "%host%?api_key=%apikey%&page=%page%&per_page=%per_page%&context=embed"
+curl -o "get_ebooks_embed_RESPONSE.json" -L -g -k -X GET "%host%?api_key=%apikey%&page=%page%&per_page=%per_page%&context=$context"
 
 timeout /t 30
