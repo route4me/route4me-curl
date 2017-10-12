bfc67db704ae306aed21e2bfd833069cf5c03bac
Amazos <olegi_g@yahoo.com>
2017-10-12 20:13:51 +0400

    added Telematics Calls group examples

--- Telematics/Windows/Telematics Calls/get_telematics_connections_vehicles.bat
+++ Telematics/Windows/Telematics Calls/get_telematics_connections_vehicles.bat
@@ -3,12 +3,14 @@
 SET url=https://production-api-telematics.route4me.com/api
 
 SET endpoint=/connections/
-SET apitoken=11111111111111111111111111111111
-SET member_id=17
+
+SET connectionToken=11111111111111111111111111111111
+SET apiToken=22222222222222222222222222222222
+
 
 :: The example refers to the process of getting telematics connection's vehicles
 
 ECHO ON
-curl -o get_telematics_connections_vehicles_RESPONSE.json -H "Accept: text/html" -g -X GET -k "%url%%endpoint%%apitoken%/vehicles" 
+curl -o get_telematics_connections_vehicles_RESPONSE.json -H "Accept: text/html" -g -X GET -k "%url%%endpoint%%connectionToken%/vehicles?api_token=%apiToken%" 
 
 timeout /t 30
