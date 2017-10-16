6c431d9d417c54e809fb6c722faf24b9e330b526
Amazos <olegi_g@yahoo.com>
2017-10-16 11:40:56 +0400

    updated  Telematics Calls examples

--- Telematics/Windows/Telematics Calls/get_telematics_vehicles_locations.bat
+++ Telematics/Windows/Telematics Calls/get_telematics_vehicles_locations.bat
@@ -4,8 +4,8 @@ SET url=https://production-api-telematics.route4me.com/api
 
 SET endpoint=/connections/
 
-SET connectionToken=11111111111111111111111111111111
-SET apiToken=22222222222222222222222222222222
+SET connectionToken=22222222222222222222222222222222
+SET apiToken=11111111111111111111111111111111
 
 
 :: The example refers to the process of getting if the locations of the telematics connection's vehicles. 
