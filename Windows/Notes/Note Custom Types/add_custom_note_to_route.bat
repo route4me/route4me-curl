aef08fbe3e5681a58c413b4dfdc7df9b7493035b
Amazos <olegi_g@yahoo.com>
2018-03-27 19:41:55 +0400

    updated notes examples

--- Windows/Notes/Note Custom Types/add_custom_note_to_route.bat
+++ Windows/Notes/Note Custom Types/add_custom_note_to_route.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-SET url=https://www.route4me.com/actions/addRouteNotes.php
+SET url=https://api.route4me.com/actions/addRouteNotes.php
 
 SET apikey=11111111111111111111111111111111
 SET routeID=D1DF6BAF991E3CDC00134C28D27AEC12
