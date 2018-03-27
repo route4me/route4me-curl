bb57f1dc7925a31fae0b9d498499b1698c30bc0e
Amazos <olegi_g@yahoo.com>
2018-03-27 19:32:01 +0400

    updated members examples

--- Windows/Members/member_create_v4.bat
+++ Windows/Members/member_create_v4.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-SET url=https://route4me.com/api.v4/user.php
+SET url=https://api.route4me.com/api.v4/user.php
 SET apikey=11111111111111111111111111111111
 
 :: The example refers to the process of creating a sub-user in a member's account
--- Windows/Members/webinar_registration.bat
+++ Windows/Members/webinar_registration.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-SET url=https://www.route4me.com/actions/webinar_register.php
+SET url=https://api.route4me.com/actions/webinar_register.php
 SET apikey=11111111111111111111111111111111
 
 ECHO ON
