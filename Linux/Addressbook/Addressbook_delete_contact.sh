81e08d29f75881763fe5192ec876cd3c9e43e885
Amazos <olegi_g@yahoo.com>
2018-03-28 13:05:16 +0400

    updated addressbook examples (linux (bash))

--- Linux/Addressbook/Addressbook_delete_contact.sh
+++ Linux/Addressbook/Addressbook_delete_contact.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-url=http://www.route4me.com/api.v4/address_book.php
+url=http://api.route4me.com/api.v4/address_book.php
 apikey=11111111111111111111111111111111
 
 # The example refers to the process of removing a location from an address book belonged to an user.
