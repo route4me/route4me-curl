ba01911875cdf4ae9469ca1a5d5b5a365792116b
Amazos <olegi_g@yahoo.com>
2018-03-28 13:09:25 +0400

    updated route addess examples (linux (bash))

--- Linux/Addresses/mark_address_as_departed.sh
+++ Linux/Addresses/mark_address_as_departed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-url=https://www.route4me.com/api/route/mark_address_departed.php
+url=https://api.route4me.com/api/route/mark_address_departed.php
 apikey=11111111111111111111111111111111
 routeid=5C15E83A4BE005BCD1537955D28D51D7
 addressid=160940135
