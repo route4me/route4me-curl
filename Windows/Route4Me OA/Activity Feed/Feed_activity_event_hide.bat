f2eab45762a8c27a0f05482117e7c4294755ae14
Amazos <olegi_g@yahoo.com>
2018-07-24 12:41:06 +0400

    updated Feed_activity_event_hide.bat

--- Windows/Route4Me OA/Activity Feed/Feed_activity_event_hide.bat
+++ Windows/Route4Me OA/Activity Feed/Feed_activity_event_hide.bat
@@ -2,7 +2,6 @@ ECHO OFF
 
 :: Please, put real values n the parameters originApiKey and authToken
 SET url=https://oa.route4me.com/api/v1/feed/event/
-SET originApiKey=55555555555555555555555555555555
 SET authToken=111111111111111111111111111111111111.222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222.333333333-44444444444444444444444444444-555
 SET event_id=44444444444444444444444444444444
 SET hide=true
@@ -11,6 +10,6 @@ SET hide=true
 
 ECHO ON
 
-curl -o Feed_activity_event_hide_RESPONSE.json -H "Accept: application/json" -g -k -X GET  "%url%%event_id%?original_api_key=%originApiKey%&auth_token=%authToken%&hide=%hide%"
+curl -o Feed_activity_event_hide_RESPONSE.json -H "Accept: application/json" -g -k -X GET  "%url%%event_id%?auth_token=%authToken%&hide=%hide%"
 
 timeout /t 30
