4147553e9dc191a4b14056116cbdeeb2e3ec4c3c
Amazos <olegi_g@yahoo.com>
2018-07-25 14:40:49 +0400

    created Statistics_stops_detected example

---
+++ Windows/Route4Me OA/Statistics/Statistics_stops_detected.bat
@@ -0,0 +1,15 @@
+ECHO OFF
+
+:: Please, put real values n the parameters originApiKey and authToken
+SET url=https://oa.route4me.com/api/v1/statistics/stop/detected/count-grouped
+SET authToken=111111111111111111111111111111111111.222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222.333333333-44444444444444444444444444444-555
+SET from_ts=1526649668
+SET trim_ts=1529328068
+
+:: The example refers to the process of getting the statistics of the detected stops
+
+ECHO ON
+
+curl -o Statistics_stops_detected_RESPONSE.json -H "Accept: application/json" -g -k -X GET  "%url%?auth_token=%authToken%&from_ts=%from_ts%&trim_ts=%trim_ts%"
+
+timeout /t 30
