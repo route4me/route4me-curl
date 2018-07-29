d6791d49e0f5514707248988633028c227698f91
Amazos <olegi_g@yahoo.com>
2018-07-29 14:28:40 +0400

    renamed User_members_devices example

--- Windows/Route4Me OA/User/User_members_devices.bat
+++ Windows/Route4Me OA/User/User_members_devices.bat
@@ -5,10 +5,10 @@ SET url=https://oa.route4me.com/api/v1/user/members/devices
 SET xAuthToken=eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ.LL-gggggggggggggggggggggggggggggggggggggggg
 
 
-:: The example refers to the process of getting the member's devices
+:: The example refers to the process of getting the members devices
 
 ECHO ON
 
-curl -o User_member_devices_RESPONSE.json -H "x-auth-token: %xAuthToken%" -H "Accept: application/json" -g -k -X GET  "%url%"
+curl -o User_members_devices_RESPONSE.json -H "x-auth-token: %xAuthToken%" -H "Accept: application/json" -g -k -X GET  "%url%"
 
 timeout /t 30
