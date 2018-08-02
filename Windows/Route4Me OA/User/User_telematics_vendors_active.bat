6d6b2ffbca0c8f9ca5a0f20420c45ff34083d5be
Amazos <olegi_g@yahoo.com>
2018-08-02 14:15:42 +0400

    created User_telematics_vendors_active example

---
+++ Windows/Route4Me OA/User/User_telematics_vendors_active.bat
@@ -0,0 +1,14 @@
+ECHO OFF
+
+:: Please, put real values in the parameter xAuthToken
+SET url=https://oa.route4me.com/api/v1/user/vendors/active
+SET xAuthToken=eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ.LL-gggggggggggggggggggggggggggggggggggggggg
+
+
+:: The example refers to the process of getting the active telematics vendors
+
+ECHO ON
+
+curl -o User_telematics_vendors_active_RESPONSE.json -H "x-auth-token: %xAuthToken%" -H "Accept: application/json" -g -k -X GET  "%url%"
+
+timeout /t 30
