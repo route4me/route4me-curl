f60cc16ca90ff168f5460079cc9aee8c70d2599e
Amazos <olegi_g@yahoo.com>
2018-08-03 15:49:43 +0400

    updated Report_settings_delete example

--- Windows/Route4Me OA/Report Settings/Report_settings_delete.bat
+++ Windows/Route4Me OA/Report Settings/Report_settings_delete.bat
@@ -2,8 +2,8 @@ ECHO OFF
 
 :: Please, put real values in the parameter xAuthToken
 SET url=https://oa.route4me.com/api/v1/report/settings/
-SET xAuthToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJFQkE3OTQzNTREQzg0N0UzQkMxOUFFRTQ5Q0Y0QUIxQiIsImV4cCI6MTUzMzc0NDI4NSwiaWF0IjoxNTMzMTM5NDg1fQ.bpScknYa4B9XcGa-P6zoeIzMTdT2N1wBgm-B434peZk
-SET user_id=7F9590B72CC941A49CD5348FE6002E04
+SET xAuthToken=eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ.LL-gggggggggggggggggggggggggggggggggggggggg
+SET user_id=7F9590B72CCccccccccccccccccccccc
 
 
 :: The example refers to the process of deleting the report settings
