dea590157f78ca430b62ce7bf55b24353460bd79
Amazos <olegi_g@yahoo.com>
2018-08-06 12:27:24 +0400

    created Autid_members_detected_routes_get example

---
+++ Windows/Route4Me OA/Audit/Autid_members_detected_routes_get.bat
@@ -0,0 +1,16 @@
+ECHO OFF
+
+:: Please, put real values in the parameter xAuthToken
+SET url=https://oa.route4me.com/api/v1/audit/members/
+SET xAuthToken=eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ.LL-gggggggggggggggggggggggggggggggggggggggg
+SET r4m_member_id=888888
+SET timestamp_from=1526649668
+SET timestamp_to=1529328068
+
+:: The example refers to the process of getting all detected routes by the member_id
+
+ECHO ON
+
+curl -o Autid_members_detected_routes_get_RESPONSE.json -H "x-auth-token: %xAuthToken%" -H "Accept: application/json" -g -k -X GET "%url%%r4m_member_id%/detected-routes?timestamp_from=%timestamp_from%&timestamp_to=%timestamp_to%"
+
+timeout /t 30
