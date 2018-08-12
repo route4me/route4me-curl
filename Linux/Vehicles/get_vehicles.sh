2fcaa9de83aff7ed7625ca68b47557c2b807ae98
Amazos <olegi_g@yahoo.com>
2018-08-12 20:14:28 +0400

    create Vehicles curl (bash) examples

--- Linux/Vehicles/get_vehicles.sh
+++ Linux/Vehicles/get_vehicles.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-url=https://api.route4me.com/api/vehicles/view_vehicles.php
+url=https://wh.route4me.com/modules/api/vehicles
 apikey=11111111111111111111111111111111
 
 # The example refers to the process of getting a list of the vehicles.
 
-curl -o file1.txt -g -X GET -k "$url?api_key=$apikey"
+curl -o get_vehicles_RESPONSE.json -g -X GET -k "$url?api_key=$apikey"
 
 echo "Finished..."
 
