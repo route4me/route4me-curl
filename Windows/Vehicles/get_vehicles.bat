c31c38fec3bf4b7a01efb459c18b1630df96d3da
Amazos <olegi_g@yahoo.com>
2018-08-12 19:48:13 +0400

    created Vehicles curl examples

--- Windows/Vehicles/get_vehicles.bat
+++ Windows/Vehicles/get_vehicles.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 
-SET url=https://api.route4me.com/api/vehicles/view_vehicles.php
+SET url=https://wh.route4me.com/modules/api/vehicles
 SET apikey=11111111111111111111111111111111
 
 :: The example refers to the process of getting a list of the vehicles.
 
 ECHO ON
 
-curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%"
+curl -o get_vehicles_RESPONSE.json -g -X GET -k "%url%?api_key=%apikey%"
 
 timeout /t 30
