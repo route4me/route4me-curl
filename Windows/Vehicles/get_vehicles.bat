141c5e8dfb34ffbd010c5712000f4dbf4a486cc2
Amazos <olegi_g@yahoo.com>
2018-08-15 13:50:43 +0400

    updated get_vehicles example

--- Windows/Vehicles/get_vehicles.bat
+++ Windows/Vehicles/get_vehicles.bat
@@ -2,11 +2,14 @@
 
 SET url=https://wh.route4me.com/modules/api/vehicles
 SET apikey=11111111111111111111111111111111
+SET with_pagination=true
+SET page=2
+SET perPage=10
 
 :: The example refers to the process of getting a list of the vehicles.
 
 ECHO ON
 
-curl -o get_vehicles_RESPONSE.json -g -X GET -k "%url%?api_key=%apikey%"
+curl -o get_vehicles_RESPONSE.json -g -X GET -k "%url%?api_key=%apikey%&with_pagination=%with_pagination%&page=%page%&perPage=%perPage%"
 
 timeout /t 30
