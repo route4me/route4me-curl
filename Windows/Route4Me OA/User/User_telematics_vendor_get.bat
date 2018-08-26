f89a8d2395ec7f2a78166d9a356abc7c4e7c9071
Amazos <olegi_g@yahoo.com>
2018-08-26 20:23:16 +0400

    created RD_proxy_analyzed_route_get example

--- Windows/Route4Me OA/User/User_telematics_vendor_get.bat
+++ Windows/Route4Me OA/User/User_telematics_vendor_get.bat
@@ -3,13 +3,13 @@ ECHO OFF
 :: Please, put real values in the parameter xAuthToken
 SET url=https://oa.route4me.com/api/v1/user/vendors/
 SET xAuthToken=eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ.LL-gggggggggggggggggggggggggggggggggggggggg
-SET vendor_id=F9E0EC73944444444444444444444444
+SET user_id=F9E0EC73944444444444444444444444
 
 
-:: The example refers to the process of getting a telematics vendor
+:: The example refers to the process of getting a telematics vendors belonging to the user.
 
 ECHO ON
 
-curl -o User_telematics_vendor_get_RESPONSE.json -H "x-auth-token: %xAuthToken%" -H "Accept: application/json" -g -k -X GET  "%url%%vendor_id%"
+curl -o User_telematics_vendor_get_RESPONSE.json -H "x-auth-token: %xAuthToken%" -H "Accept: application/json" -g -k -X GET  "%url%%user_id%"
 
 timeout /t 30
