196714da5e54857be066abce317ac8e3681d16aa
Amazos <olegi_g@yahoo.com>
2018-08-29 00:25:07 +0400

    updated User_telematics_vendor_update.bat

--- Windows/Route4Me OA/User/User_telematics_vendor_update.bat
+++ Windows/Route4Me OA/User/User_telematics_vendor_update.bat
@@ -1,14 +1,14 @@
 ECHO OFF
 
-:: Please, put real values in the parameter xAuthToken
+:: Please, put real value in the parameter xAuthToken
 SET url=https://oa.route4me.com/api/v1/user/vendors/
 SET xAuthToken=eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ.LL-gggggggggggggggggggggggggggggggggggggggg
-SET vendor_id=F9E0EC73944444444444444444444444
+SET user_id=F9E0EC73944444444444444444444444
 
 :: The example refers to the process of updating a teleamtics vendor
 
 ECHO ON
 
-curl -o User_telematics_vendor_update_RESPONSE.json -H "Content-Type: application/json" -H "Accept: application/json" -H "x-auth-token: %xAuthToken%" -g -k -X PUT -d "@User_telematics_vendor_update_data.json" "%url%%vendor_id%"
+curl -o User_telematics_vendor_update_RESPONSE.json -H "Content-Type: application/json" -H "Accept: application/json" -H "x-auth-token: %xAuthToken%" -g -k -X PUT -d "@User_telematics_vendor_update_data.json" "%url%%user_id%"
 
 timeout /t 30
