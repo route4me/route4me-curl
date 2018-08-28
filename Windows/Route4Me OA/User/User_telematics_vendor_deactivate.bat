b37c465ed8a0524e1c56b186c2b3a81b2cbf7b23
Amazos <olegi_g@yahoo.com>
2018-08-29 00:16:13 +0400

    update User_telematics_vendor_deactivate.bat

--- Windows/Route4Me OA/User/User_telematics_vendor_deactivate.bat
+++ Windows/Route4Me OA/User/User_telematics_vendor_deactivate.bat
@@ -3,13 +3,13 @@ ECHO OFF
 :: Please, put real values in the parameter xAuthToken
 SET url=https://oa.route4me.com/api/v1/user/vendors/
 SET xAuthToken=eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ.LL-gggggggggggggggggggggggggggggggggggggggg
-SET vendor_id=F9E0EC73944444444444444444444444
+SET user_id=F9E0EC73944444444444444444444444
 
 
 :: The example refers to the process of activating/deactivating of a telematics vendor
 
 ECHO ON
 
-curl -o User_telematics_vendor_deactivate_RESPONSE.json -H "Content-Type: application/json" -H "x-auth-token: %xAuthToken%" -H "Accept: application/json" -g -k -X PATCH -d "@User_telematics_vendor_deactivate_data.json" "%url%%vendor_id%"
+curl -o User_telematics_vendor_deactivate_RESPONSE.json -H "Content-Type: application/json" -H "x-auth-token: %xAuthToken%" -H "Accept: application/json" -g -k -X PATCH -d "@User_telematics_vendor_deactivate_data.json" "%url%%user_id%"
 
 timeout /t 30
