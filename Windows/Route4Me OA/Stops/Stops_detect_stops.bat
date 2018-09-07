0aae5aae984817acbc88e9a1f1280d869c1aad1e
Amazos <olegi_g@yahoo.com>
2018-09-07 16:00:30 +0400

    renamed Points to Stops

---
+++ Windows/Route4Me OA/Stops/Stops_detect_stops.bat
@@ -0,0 +1,13 @@
+ECHO OFF
+
+:: Please, put real values in the parameter apiKey
+SET url=https://oa.route4me.com/api/v1/stops/detect
+SET apiKey=c8f031c712715c90b2e0cd1ce19cabf24792eb2d297a85346fa10df16842f40f
+
+:: The example refers to the process of detecting of the vehicle stops
+
+ECHO ON
+
+curl -o Stops_detect_stops_RESPONSE.json -H "Content-Type: application/json" -H "Accept: application/json" -g -k -X POST -d "@Stops_detect_stops_data.json" "%url%?api_key=%apiKey%"
+
+timeout /t 30
