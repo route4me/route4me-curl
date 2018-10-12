dd08eb36070e2aff7d3acf877c7b208f18ed147e
Amazos <olegi_g@yahoo.com>
2018-10-12 14:43:18 +0400

    updated Stops example

--- Windows/Route4Me OA/Stops/Stops_detect_stops.bat
+++ Windows/Route4Me OA/Stops/Stops_detect_stops.bat
@@ -2,7 +2,7 @@ ECHO OFF
 
 :: Please, put real values in the parameter apiKey
 SET url=https://oa.route4me.com/api/v1/stops/detect
-SET apiKey=c8f031c712715c90b2e0cd1ce19cabf247922222222222222222222222222222
+SET api_key=1111111111111111111111111111111111111111111111111111111111111111
 
 :: The example refers to the process of detecting of the vehicle stops
 
