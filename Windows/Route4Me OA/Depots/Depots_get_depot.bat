a072d488a3a56c627ae62dadba4fae7ce30ac103
Amazos <olegi_g@yahoo.com>
2018-10-12 14:14:28 +0400

    updated Depots examples

--- Windows/Route4Me OA/Depots/Depots_get_depot.bat
+++ Windows/Route4Me OA/Depots/Depots_get_depot.bat
@@ -1,14 +1,14 @@
 ECHO OFF
 
-:: Please, put real values in the parameter xAuthToken
+:: Please, put real value in the parameter api_key
 SET url=https://oa.route4me.com/api/v1/depot/
-SET xAuthToken=eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ.LL-gggggggggggggggggggggggggggggggggggggggg
+SET api_key=1111111111111111111111111111111111111111111111111111111111111111
 SET depot_id=0E76BE2F7EEEEEEEEEEEEEEEEEEEEEEE
 
 :: The example refers to the process of getting a depot
 
 ECHO ON
 
-curl -o Depots_get_depot_RESPONSE.json -H "x-auth-token: %xAuthToken%" -H "Accept: application/json" -g -k -X GET "%url%%depot_id%"
+curl -o Depots_get_depot_RESPONSE.json -H "Accept: application/json" -g -k -X GET "%url%%depot_id%?api_key=%api_key%"
 
 timeout /t 30
