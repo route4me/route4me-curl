84ba67b29317e675541c2372556e8d3be1cc94df
Amazos <olegi_g@yahoo.com>
2018-10-12 14:11:31 +0400

    updated audit examples

--- Windows/Route4Me OA/Audit/Audit_members_detected_routes_get.bat
+++ Windows/Route4Me OA/Audit/Audit_members_detected_routes_get.bat
@@ -1,8 +1,8 @@
 ECHO OFF
 
-:: Please, put real values in the parameter xAuthToken
+:: Please, put real value in the parameter api_key
 SET url=https://oa.route4me.com/api/v1/audit/members/
-SET xAuthToken=eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ.LL-gggggggggggggggggggggggggggggggggggggggg
+SET api_key=1111111111111111111111111111111111111111111111111111111111111111
 SET r4m_member_id=888888
 SET timestamp_from=1526649668
 SET timestamp_to=1529328068
@@ -11,6 +11,6 @@ SET timestamp_to=1529328068
 
 ECHO ON
 
-curl -o Audit_members_detected_routes_get_RESPONSE.json -H "x-auth-token: %xAuthToken%" -H "Accept: application/json" -g -k -X GET "%url%%r4m_member_id%/detected-routes?timestamp_from=%timestamp_from%&timestamp_to=%timestamp_to%"
+curl -o Audit_members_detected_routes_get_RESPONSE.json -H "Accept: application/json" -g -k -X GET "%url%%r4m_member_id%/detected-routes?api_key=%api_key%&timestamp_from=%timestamp_from%&timestamp_to=%timestamp_to%"
 
 timeout /t 30
