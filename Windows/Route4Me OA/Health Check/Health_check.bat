18062ddd2e8fcb3fc6205403ffc293ce9345edca
Amazos <olegi_g@yahoo.com>
2018-10-12 14:15:54 +0400

    updated Health check example

--- Windows/Route4Me OA/Health Check/Health_check.bat
+++ Windows/Route4Me OA/Health Check/Health_check.bat
@@ -1,13 +1,13 @@
 ECHO OFF
 
-:: Please, put the real value in the parameter xAuthToken
+:: Please, put the real value in the parameter api_key
 SET url=https://oa.route4me.com/api/v1/health/check
-SET xAuthToken=eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ.LL-gggggggggggggggggggggggggggggggggggggggg
+SET api_key=1111111111111111111111111111111111111111111111111111111111111111
 
 :: The example refers to the process of checking of the system health
 
 ECHO ON
 
-curl -o Health_check_RESPONSE.json -H "x-auth-token: %xAuthToken%" -H "Accept: application/json" -g -k -X GET  "%url%"
+curl -o Health_check_RESPONSE.json -H "Accept: application/json" -g -k -X GET  "%url%?api_key=%api_key%"
 
 timeout /t 30
