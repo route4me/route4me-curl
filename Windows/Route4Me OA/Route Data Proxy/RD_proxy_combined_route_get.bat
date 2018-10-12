86fb5f318e7b609c998b6f0351458ebcb8b8ad07
Amazos <olegi_g@yahoo.com>
2018-10-12 14:21:49 +0400

    updated Route Data Proxy examples

--- Windows/Route4Me OA/Route Data Proxy/RD_proxy_combined_route_get.bat
+++ Windows/Route4Me OA/Route Data Proxy/RD_proxy_combined_route_get.bat
@@ -2,7 +2,7 @@ ECHO OFF
 
 :: Please, put the real value in the parameter api_key
 SET url=https://oa.route4me.com/api/v1/route-data-proxy/combined/
-SET api_key=c8f031c712715c90b2e0cd1ce19cabf24792eb22222222222222222222222222
+SET api_key=1111111111111111111111111111111111111111111111111111111111111111
 SET analyzed_route_id=4FE9DD79D5FA466CCCCCCCCCCCCCCCCC
 
 :: The example refers to the process of getting of an combined analyzed route
