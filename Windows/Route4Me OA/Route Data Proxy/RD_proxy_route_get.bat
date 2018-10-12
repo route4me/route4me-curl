5efa6d5dd5287b2fbf048559bfea3fa863cd7176
Amazos <olegi_g@yahoo.com>
2018-10-12 14:21:49 +0400

    updated Route Data Proxy examples

--- Windows/Route4Me OA/Route Data Proxy/RD_proxy_route_get.bat
+++ Windows/Route4Me OA/Route Data Proxy/RD_proxy_route_get.bat
@@ -2,7 +2,7 @@ ECHO OFF
 
 :: Please, put the real value in the parameter api_key
 SET url=https://oa.route4me.com/api/v1/route-data-proxy/
-SET api_key=c8f031c712715c90b2e0cd1ce19cabf24792eb22222222222222222222222222
+SET api_key=1111111111111111111111111111111111111111111111111111111111111111
 SET route_id=DB5E95F09FF7443335A4444444444444
 
 :: The example refers to the process of getting of a route
