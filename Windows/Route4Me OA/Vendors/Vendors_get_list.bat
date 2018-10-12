1bd9c762ef3ace6cd78388dc589075acdd669e82
Amazos <olegi_g@yahoo.com>
2018-10-12 15:33:28 +0400

    updated Vendors example

--- Windows/Route4Me OA/Vendors/Vendors_get_list.bat
+++ Windows/Route4Me OA/Vendors/Vendors_get_list.bat
@@ -1,13 +1,13 @@
 ECHO OFF
 
-:: Please, put the real value in the parameter xAuthToken
+:: Please, put the real value in the parameter api_key
 SET url=https://oa.route4me.com/api/v1/vendors
-SET xAuthToken=eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ.LL-gggggggggggggggggggggggggggggggggggggggg
+SET api_key=1111111111111111111111111111111111111111111111111111111111111111
 
 :: The example refers to the process of getting the vendors list
 
 ECHO ON
 
-curl -o Vendors_get_list_RESPONSE.json -H "x-auth-token: %xAuthToken%" -H "Accept: application/json" -g -k -X GET  "%url%"
+curl -o Vendors_get_list_RESPONSE.json -H "Accept: application/json" -g -k -X GET "%url%?api_key=%api_key%"
 
 timeout /t 30
