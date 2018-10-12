9e5770bf86c3e24e21728be1fee72d303217ba44
Amazos <olegi_g@yahoo.com>
2018-10-12 14:28:14 +0400

    updated Statistics examples

--- Windows/Route4Me OA/Statistics/Statistics_gps_tracks_received.bat
+++ Windows/Route4Me OA/Statistics/Statistics_gps_tracks_received.bat
@@ -1,8 +1,8 @@
 ECHO OFF
 
-:: Please, put real values n the parameters originApiKey and authToken
+:: Please, put real value in the parameter api_key
 SET url=https://oa.route4me.com/api/v1/statistics/gps-track/received/count-grouped
-SET authToken=111111111111111111111111111111111111.222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222.333333333-44444444444444444444444444444-555
+SET api_key=1111111111111111111111111111111111111111111111111111111111111111
 SET from_ts=1526649668
 SET trim_ts=1529328068
 
@@ -10,6 +10,6 @@ SET trim_ts=1529328068
 
 ECHO ON
 
-curl -o Statistics_gps_tracks_received_RESPONSE.json -H "Accept: application/json" -g -k -X GET  "%url%?auth_token=%authToken%&from_ts=%from_ts%&trim_ts=%trim_ts%"
+curl -o Statistics_gps_tracks_received_RESPONSE.json -H "Accept: application/json" -g -k -X GET  "%url%?api_key=%api_key%&from_ts=%from_ts%&trim_ts=%trim_ts%"
 
 timeout /t 30
