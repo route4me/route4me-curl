3b2400bb11dd4b67cc5204c8accb08d405c43708
olman62 <oleg.guchi@gmail.com>
2019-06-30 14:28:30 +0400

    created Address Book Group files

---
+++ Linux/Addressbook/Address Book Group/get_addressbook_groups.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# The example refers to the process of getting limited number of the address book groups.
+
+url=https://www.route4me.com/api.v4/address_book_group.php
+apikey=11111111111111111111111111111111
+offset=0
+limit=10
+
+curl -o get_addressbook_groups_RESPONSE.json -g -k -XGET "$url?api_key=$apikey&offset=$offset&limit=$limit"
+
+echo "Finished..."
+
+sleep 30
