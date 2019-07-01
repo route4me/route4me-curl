442d68affaba0547e1ace9235e6e604013dfadea
Amazos <olegi_g@yahoo.com>
2019-07-01 19:14:40 +0400

    changed www to api

--- Linux/Addressbook/AddressBookGroup/create_addressbook_group.sh
+++ Linux/Addressbook/AddressBookGroup/create_addressbook_group.sh
@@ -2,7 +2,7 @@
 
 # The example refers to the process of creating address book group.
 
-url=https://www.route4me.com/api.v4/address_book_group.php
+url=https://api.route4me.com/api.v4/address_book_group.php
 apikey=11111111111111111111111111111111
 
 curl -o create_addressbook_group_RESPONSE.json -g -k -X POST -H "Content-Type: application/json" -d "@create_addressbook_group_data.json" "$url?api_key=$apikey"
