8f3a1d8a8e21bb2180857ff893c6cb0f71e32365
Amazos <olegi_g@yahoo.com>
2019-07-02 17:24:40 +0400

    Update update_addressbook_group.bat

--- Windows/Addressbook/AddressBookGroup/update_addressbook_group.bat
+++ Windows/Addressbook/AddressBookGroup/update_addressbook_group.bat
@@ -3,7 +3,9 @@ ECHO OFF
 SET url=https://api.route4me.com/api.v4/address_book_group.php
 SET apikey=11111111111111111111111111111111
 
-:: The example refers to the process of creating address book group.
+:: The example refers to the process of updating an address book group.
+:: Note: you can find the allowed colors for the address book group at this link:
+:: https://github.com/route4me/route4me-json-schemas/blob/master/ColorSamples/AddressBookGroupAvailableColors.png 
 
 ECHO ON
 
