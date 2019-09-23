e6d01592a35ac751bf5164ac40ae040b87a80e33
olman62 <oleg.guchi@gmail.com>
2019-09-23 22:41:38 +0400

    update get_orders example

--- Windows/Orders/get_orders.bat
+++ Windows/Orders/get_orders.bat
@@ -1,11 +1,14 @@
 ECHO OFF
-SET url=http://www.route4me.com/api.v4/order.php
+
+SET url=https://api.route4me.com/api.v4/order.php
 SET apikey=11111111111111111111111111111111
+SET offset=0
+SET limit=7
 
 ECHO ON
 
 :: The example demonstrates the process of getting all orders.
 
-curl -o file1.txt -X GET "%url%?api_key=%apikey%"
+curl -o get_orders_RESPONSE.json -g -k -X GET "%url%?api_key=%apikey%&offset=%offset%&limit=%limit%"
 
 timeout /t 30
