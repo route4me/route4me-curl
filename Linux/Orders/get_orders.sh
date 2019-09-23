7b188d1f45e330315dd3c52f6170bce67c5a8d76
olman62 <oleg.guchi@gmail.com>
2019-09-23 22:58:30 +0400

    updated get_orders example

--- Linux/Orders/get_orders.sh
+++ Linux/Orders/get_orders.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-url=http://www.route4me.com/api.v4/order.php
+url=https://api.route4me.com/api.v4/order.php
 apikey=11111111111111111111111111111111
+offset=0
+limit=7
 
 # The example demonstrates the process of getting all orders.
 
-curl -o file1.txt -X GET "$url?api_key=$apikey"
+curl -o get_orders_RESPONSE.json -k -X GET "$url?api_key=$apikey&offset=$offset&limit=$limit"
 
 echo "Finished..."
 
