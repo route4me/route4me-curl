1dd888c9ec27f72338c980b815866a9af6be66cd
Amazos <olegi_g@yahoo.com>
2020-08-13 15:44:24 +0400

    move to the folder Windows

--- API_5/Windows/ActivityFeed/get_activities_all.bat
+++ API_5/Windows/ActivityFeed/get_activities_all.bat
@@ -3,7 +3,7 @@
 :: You should insert real test data and your API key for accurate testing
 
 SET url=https://wh.route4me.com/modules/api/v5.0/activity/feed
-SET apikey=11111111111111111111111111111111
+SET apikey=51d0c0701ce83855c9f62d0440096e7c
 SET start=0
 SET end=0
 SET offset=0
