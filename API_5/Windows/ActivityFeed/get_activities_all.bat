c27bd1fca4cc77d8f880d11b8c94b0757f156eb9
Amazos <olegi_g@yahoo.com>
2020-08-14 15:07:57 +0400

    Update get_activities_all.bat

--- API_5/Windows/ActivityFeed/get_activities_all.bat
+++ API_5/Windows/ActivityFeed/get_activities_all.bat
@@ -2,14 +2,15 @@
 
 :: You should insert real test data and your API key for accurate testing
 
-SET url=https://wh.route4me.com/modules/api/v5.0/activity/feed
-SET apikey=51d0c0701ce83855c9f62d0440096e7c
-SET start=0
-SET end=0
+SET base_url=https://wh.route4me.com/modules/api/v5.0
+SET endpoint=/activity/feed
+
+SET apikey=11111111111111111111111111111111
+
 SET offset=0
 SET limit=40
 
 ECHO ON
-curl -o get_activities_all_RESPONSE.json -g -k -X GET "%URL%?offset=%offset%&limit=%limit%&start=%start%&end=%end%"
+curl -o get_activities_all_RESPONSE.json -g -k -X GET "%base_url%%base_url%?offset=%offset%&limit=%limit%"
 
 timeout /t 30
