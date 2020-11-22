1ed8d33887cc7c8c915e29d0d1a8afce2ea6fa4a
olman62 <oleg.guchi@gmail.com>
2020-11-22 20:38:21 +0400

    added DriverReview and DriverReviewSettings examples
    curl bush

---
+++ API_5/Linux/DriverRating/DriverReviewSettings/upload_driver_review_settings.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+base_url=https://wh.route4me.com/modules/api/v5.0
+endpoint=/driver-reviews/settings/upload
+
+apikey=11111111111111111111111111111111
+
+# The example refers to the process of uploading the driver review settings to the account.
+
+curl -o upload_driver_review_settings_RESPONSE.json -g -X POST -k -H "accept: application/json" -H "Content-Type: application/json" -d "@upload_driver_review_settings_data.json" "$base_url$endpoint?api_key=$apikey"
+
+echo "Finished..."
+
+sleep 15
