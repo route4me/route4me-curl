24ae0996759b653ee9d5df8ee03543113b0923c2
Amazos <oleg.guchi@gmail.com>
2021-04-03 18:10:34 +0400

    add Bugs folder

---
+++ Bugs/GeocodeFileUploading/upload_file_geocoding.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+SET url=https://www.route4me.com/actions/upload/csv-xls-geocode.php
+SET apikey=11111111111111111111111111111111
+SET upload_id=7f371a10cc5318a732b1cdaa5bbc79b5
+
+ECHO ON
+
+curl -o file1.txt -g -k -X POST "%url%?api_key=%apikey%&strUploadID=%upload_id%" 
+
+:: In more complex case, yo can define valid and ignore column names:
+
+:: curl -o file1.txt -g -k -X POST -d "@upload_file_geocoding_data.json" "%url%?api_key=%apikey%&strUploadID=%upload_id%" 
+
+timeout /t 30
