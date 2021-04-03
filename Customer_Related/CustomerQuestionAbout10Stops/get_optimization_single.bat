050e7a99563ba51b539164d38fa5b5141a3ccca2
Amazos <oleg.guchi@gmail.com>
2021-04-03 18:14:09 +0400

    add Customer Related folder

---
+++ Customer_Related/CustomerQuestionAbout10Stops/get_optimization_single.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+SET url=https://www.route4me.com/api.v4/optimization_problem.php
+SET apikey=11111111111111111111111111111111
+SET optprobid=06E463409A50EF82302F51BD3D51D15F
+
+ECHO ON
+curl -o file1.txt -g -X GET -k "%URL%?api_key=%apikey%&optimization_problem_id=%optprobid%" 
+
+timeout /t 30
