0d4b0f8c986d804595f90f4a7f687ef99546707d
olman62 <oleg.guchi@gmail.com>
2021-04-03 22:56:53 +0400

    update curl examples

--- UseCase/Orders2Optimization/create_new_optimization.bat
+++ UseCase/Orders2Optimization/create_new_optimization.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-SET url=http://www.broono.com/api.v4/optimization_problem.php
+SET url=https://api.route4me.com/api.v4/optimization_problem.php
 SET apikey=11111111111111111111111111111111
 
 ECHO ON
