56c44fbb26ffbcf9aa7930dfbdcaeab5b88b7786
Amazos <oleg.guchi@gmail.com>
2021-04-03 16:51:22 +0400

    add Activities folder

---
+++ Windows/Activities/get_activities_driver_arrived_early.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+:: Example refers to the process of getting all driver-arrived-early activities
+
+SET url=https://api.route4me.com/api/get_activities.php
+SET apikey=11111111111111111111111111111111
+SET act_type=driver-arrived-early
+
+ECHO ON
+curl -o file1.txt -g -k -X GET "%URL%?api_key=%apikey%&activity_type=%act_type%"
+
+timeout /t 30
