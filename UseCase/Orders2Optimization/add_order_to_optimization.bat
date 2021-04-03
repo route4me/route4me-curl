263833962f4cc7002b95c39aa3504066e9f8a287
olman62 <oleg.guchi@gmail.com>
2021-04-03 22:56:53 +0400

    update curl examples

--- UseCase/Orders2Optimization/add_order_to_optimization.bat
+++ UseCase/Orders2Optimization/add_order_to_optimization.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-SET url=http://broono.com/api.v4/optimization_problem.php
+SET url=https://api.route4me.com/api.v4/optimization_problem.php
 SET apikey=11111111111111111111111111111111
 SET rdirect=0
 SET optid=896C4E5466213841BB9AEF3EEF5D7F48
@@ -9,6 +9,6 @@ ECHO ON
 
 :: The example refers to the process of adding an order to an optimization by sending HTPP PUT data
 
-curl -o file1.txt -g -X PUT -H "Content-Type: application/json" -d "@add_order_to_route_data.json" "%url%?api_key=%apikey%&redirect=%rdirect%&optimization_problem_id=%optid%"
+curl -o file1.txt -g -k -X PUT -H "Content-Type: application/json" -d "@add_order_to_optimization_data.json" "%url%?api_key=%apikey%&redirect=%rdirect%&optimization_problem_id=%optid%"
 
 timeout /t 30
