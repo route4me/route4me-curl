876689262dc9606e4c2a1bf877eb19f16f9ca656
Amazos <oleg.guchi@gmail.com>
2021-04-03 18:14:09 +0400

    add Customer Related folder

---
+++ Customer_Related/AlgorithmTypeExamples/generate_optimization.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+
+SET url=https://api.route4me.com/api.v4/optimization_problem.php
+SET apikey=11111111111111111111111111111111
+
+ECHO ON
+
+curl -o output_data.json -g -X POST -k -d "@input_data.json" "%url%?api_key=%apikey%" 
+
+
+timeout /t 30
