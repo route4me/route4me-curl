6caee2d89f4842321783d4f75956233e2d52bf52
olman62 <oleg.guchi@gmail.com>
2021-06-03 17:58:06 +0400

    Update bulk_geocoding.bat

--- Windows/Geocoding/bulk_geocoding.bat
+++ Windows/Geocoding/bulk_geocoding.bat
@@ -4,7 +4,7 @@ SET apikey=11111111111111111111111111111111
 
 ECHO ON
 
-:: This task requires clarification- it refuses to work because API key has exceeded its maximum number of requests
+:: This endpoint is obsolete
 
 curl -o file1.txt -g -X POST -d '@bulk_geocoding_data.json' "%URL%?api_key=%apikey%" 
 
