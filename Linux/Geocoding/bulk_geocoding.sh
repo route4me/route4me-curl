53673f50cffaf92713604c90a97b1f8fc1b4894f
olman62 <oleg.guchi@gmail.com>
2021-06-03 17:59:08 +0400

    Update bulk_geocoding.sh

--- Linux/Geocoding/bulk_geocoding.sh
+++ Linux/Geocoding/bulk_geocoding.sh
@@ -4,8 +4,7 @@ url=http://api.route4me.com/actions/upload/json-geocode.php
 apikey=11111111111111111111111111111111
 
 
-# This task requires clarification- it refuses to work because API key has exceeded its maximum number of requests
-
+# The endpoint is obsolete.
 curl -o file1.txt -g -X POST -d "@bulk_geocoding_data.json" "$url?api_key=$apikey" 
 
 echo "Finished..."
