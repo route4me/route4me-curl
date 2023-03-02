6dd54551a4053140895ab23a581ab9dfe3eb967b
[Amazos] <oleg.guchi@gmail.com>
2023-03-02 19:33:27 +0400

    update Vehicles V5 curl examples

--- API_5/Windows/Vehicles/CreateAndManageVehicles/get_vehicles.bat
+++ API_5/Windows/Vehicles/CreateAndManageVehicles/get_vehicles.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 SET url=https://wh.route4me.com/modules/api/v5.0/vehicles
-SET apikey=5B05A8B6C4DB22C0CD6966F01D5FB4E0
+SET apikey=11111111111111111111111111111111
 SET show=active
 SET search_query=Medium
 
--- API_5/Windows/Vehicles/CreateAndManageVehicles/update_vehicle.bat
+++ API_5/Windows/Vehicles/CreateAndManageVehicles/update_vehicle.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 SET url=https://wh.route4me.com/modules/api/v5.0/vehicles
-SET apikey=5B05A8B6C4DB22C0CD6966F01D5FB4E0
+SET apikey=11111111111111111111111111111111
 SET vehicle_id=AE350EF3153448665300F2C8216698C6
 
 :: The example refers to the process of updating a vehicle.
